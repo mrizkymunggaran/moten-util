09a65151efbe1636a83679c988c8d466e44f0d0e
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2009-11-22 11:32:28 +0000

    find all classes in classpath package name

--- trunk/moten-util/src/test/java/moten/david/util/uml/eclipse/UmlProducerTest.java
+++ trunk/moten-util/src/test/java/moten/david/util/uml/eclipse/UmlProducerTest.java
@@ -3,6 +3,7 @@ package moten.david.util.uml.eclipse;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -22,9 +23,11 @@ public class UmlProducerTest {
 				new UmlProducerOptionsImpl(false, false));
 		OutputStream out = new FileOutputStream("target/result.uml");
 
-		String xmi = p.getUmlXmi(ClassFilter.class, ClassWrapper.class,
-				Dependency.class, UmlProducer.class,
-				UmlProducerOptionsImpl.class, ExampleClass.class);
+		List<Class> list = Util.getClasses(ClassFilter.class.getPackage()
+				.getName());
+		list.add(ExampleClass.class);
+
+		String xmi = p.getUmlXmi(list.toArray(new Class[] {}));
 
 		System.out.println(xmi);
 		out.write(xmi.getBytes());
