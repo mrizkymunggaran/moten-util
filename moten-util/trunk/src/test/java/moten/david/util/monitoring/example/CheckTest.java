bc6461cdbb71af646b18ce46da756ff447cc267b
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-02-07 11:33:30 +0000

    tests

--- moten-util/trunk/src/test/java/moten/david/util/monitoring/example/CheckTest.java
+++ moten-util/trunk/src/test/java/moten/david/util/monitoring/example/CheckTest.java
@@ -1,8 +1,10 @@
 package moten.david.util.monitoring.example;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import moten.david.util.expression.Bool;
 import moten.david.util.expression.BooleanExpression;
@@ -17,14 +19,42 @@ public class CheckTest {
 
 	@Test
 	public void test() {
-		List<Check> checks = new ArrayList<Check>();
-		BooleanExpression e = Bool.FALSE;
-		DefaultCheck check = new DefaultCheck("processing time ok", null, e,
-				Level.WARNING, null, null);
-		checks.add(check);
-		Monitor monitor = new Monitor(checks, Level.OK, Level.UNKNOWN);
-		Map<Check, moten.david.util.monitoring.Level> map = monitor.check();
-		Assert.assertEquals(Level.WARNING, map.get(check));
-
+		{
+			List<Check> checks = new ArrayList<Check>();
+			BooleanExpression e = Bool.FALSE;
+			DefaultCheck check = new DefaultCheck("processing time ok", null,
+					e, Level.WARNING, null, null);
+			checks.add(check);
+			Monitor monitor = new Monitor(checks, Level.OK, Level.UNKNOWN);
+			Map<Check, moten.david.util.monitoring.Level> map = monitor.check();
+			Assert.assertEquals(Level.WARNING, map.get(check));
+		}
+		{
+			List<Check> checks = new ArrayList<Check>();
+			BooleanExpression e = Bool.TRUE;
+			DefaultCheck check = new DefaultCheck("processing time ok", null,
+					e, Level.WARNING, null, null);
+			checks.add(check);
+			Monitor monitor = new Monitor(checks, Level.OK, Level.UNKNOWN);
+			Map<Check, moten.david.util.monitoring.Level> map = monitor.check();
+			Assert.assertEquals(Level.OK, map.get(check));
+		}
+		{
+			List<Check> checks = new ArrayList<Check>();
+			BooleanExpression e = Bool.TRUE;
+			final DefaultCheck checkBase = new DefaultCheck(
+					"base thing available", null, e, Level.SEVERE, null, null);
+			Set<Check> deps = new HashSet<Check>() {
+				{
+					add(checkBase);
+				}
+			};
+			DefaultCheck check = new DefaultCheck("processing time ok", null,
+					e, Level.WARNING, deps, null);
+			checks.add(check);
+			Monitor monitor = new Monitor(checks, Level.OK, Level.UNKNOWN);
+			Map<Check, moten.david.util.monitoring.Level> map = monitor.check();
+			Assert.assertEquals(Level.OK, map.get(check));
+		}
 	}
 }
