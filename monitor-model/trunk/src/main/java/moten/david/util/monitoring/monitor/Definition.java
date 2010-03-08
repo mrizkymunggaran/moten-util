1e29de6e14b7748177c0190e4628fa0a3ba172e4
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-03-08 09:39:11 +0000

    updated model

--- monitor-model/trunk/src/main/java/moten/david/util/monitoring/monitor/Definition.java
+++ monitor-model/trunk/src/main/java/moten/david/util/monitoring/monitor/Definition.java
@@ -3,9 +3,12 @@ package moten.david.util.monitoring.monitor;
 import model.AssociationEndPrimary;
 import model.AssociationEndSecondary;
 import model.Attribute;
+import model.AttributeReferential;
+import model.CallEvent;
 import model.Class;
-import model.Specialization;
-import model.SpecializationGroup;
+import model.Primitive;
+import model.State;
+import moten.david.xuml.model.Generator;
 import moten.david.xuml.model.Multiplicity;
 import moten.david.xuml.model.util.SystemBase;
 
@@ -18,26 +21,50 @@ public class Definition extends SystemBase {
 	private void initialize() {
 		model.Package pkg = createRootPackage("monitoring", "monitoring");
 		model.Class lookup = createClassWithArbitraryId(pkg, "Lookup", "Lookup");
-		Attribute name = createAttribute(lookup, "name");
-		Attribute time = createAttribute(lookup, "time");
-		createIdentifierNonPrimary(lookup, "I2",
-				new Attribute[] { name, time }, null);
+		Class lookupName = createClass(pkg, "LookupName", "");
+		Attribute name = createAttribute(lookupName, "name");
+		createIdentifierPrimary(name, Generator.NOT_GENERATED);
+		{
+			AssociationEndPrimary primary = createAssociationEndPrimary(lookup,
+					Multiplicity.MANY, "categorizes");
+			AssociationEndSecondary secondary = createAssociationEndSecondary(
+					lookupName, Multiplicity.ONE, "is categorized by");
+			createAssociation("R2", primary, secondary);
+			AttributeReferential ref = createAttributeReferential(lookup,
+					secondary, null);
+			Attribute time = createAttribute(lookup, "time",
+					Primitive.TIMESTAMP);
+			createIdentifierNonPrimary(lookup, "I2", new Attribute[] { time },
+					new AttributeReferential[] { ref });
+		}
 
-		SpecializationGroup lookupAvailableGroup = createSpecializationGroup(
-				lookup, "S1", "");
-		Specialization lookupAvailable = createSpecialization(
-				lookupAvailableGroup, pkg, "LookupAvailable", "");
-		Specialization lookupNotAvailable = createSpecialization(
-				lookupAvailableGroup, pkg, "LookupNotAvailable", "");
-		SpecializationGroup latestLookupGroup = createSpecializationGroup(
-				lookup, "S2", "");
-		Specialization latestLookup = createSpecialization(latestLookupGroup,
-				pkg, "LatestLookup", "");
-		createArbitraryId(latestLookup);
-
-		Specialization previousLookup = createSpecialization(latestLookupGroup,
-				pkg, "PreviousLookup", "");
-		createArbitraryId(previousLookup);
+		createAttribute(lookup, "problem").setMandatory(false);
+		CallEvent available = createCallEvent(lookup, "Available");
+		State availableState = createState(lookup, "Available");
+		createTransition(lookup.getStateMachine().getInitialState(),
+				availableState, available);
+		CallEvent addProperty = createCallEvent(lookup, "AddProperty");
+		createParameter(addProperty, "name");
+		createParameter(addProperty, "value");
+		createTransition(availableState, availableState, addProperty);
+		CallEvent notAvailable = createCallEvent(lookup, "NotAvailable");
+		State notAvailableState = createState(lookup, "NotAvailable");
+		createTransition(lookup.getStateMachine().getInitialState(),
+				notAvailableState, notAvailable);
+		CallEvent remove = createCallEvent(lookup, "Remove");
+		createTransition(availableState, lookup.getStateMachine()
+				.getFinalState(), remove);
+		createTransition(notAvailableState, lookup.getStateMachine()
+				.getFinalState(), remove);
+		{
+			{
+				AssociationEndPrimary primary = createAssociationEndPrimary(
+						lookup, Multiplicity.ZERO_ONE, "has latest");
+				AssociationEndSecondary secondary = createAssociationEndSecondary(
+						lookupName, Multiplicity.ZERO_ONE, "is latest for");
+				createAssociation("R3", primary, secondary);
+			}
+		}
 
 		Class property = createClassWithArbitraryId(pkg, "Property", "");
 		Attribute propertyName = createAttribute(property, "name");
@@ -47,8 +74,8 @@ public class Definition extends SystemBase {
 		createIdentifierNonPrimary(property, "I2", new Attribute[] {
 				propertyName, propertyValue }, null);
 		{
-			AssociationEndPrimary e1 = createAssociationEndPrimary(
-					lookupAvailable, Multiplicity.ONE, "are returned from");
+			AssociationEndPrimary e1 = createAssociationEndPrimary(lookup,
+					Multiplicity.ONE, "returned from");
 			AssociationEndSecondary e2 = createAssociationEndSecondary(
 					property, Multiplicity.MANY, "return");
 			createAssociation("R1", e1, e2);
