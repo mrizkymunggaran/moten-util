a1073292f52ea3aa76c0273f29c8bed39256687d
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-03-09 23:00:08 +0000

    instantiate ApplicationServiceProvider based on System property

--- moten-util-parent/trunk/monitor/src/main/java/moten/david/util/monitoring/gwt/server/ApplicationServiceImpl.java
+++ moten-util-parent/trunk/monitor/src/main/java/moten/david/util/monitoring/gwt/server/ApplicationServiceImpl.java
@@ -1,33 +1,57 @@
 package moten.david.util.monitoring.gwt.server;
 
+import java.util.logging.Logger;
+
 import moten.david.util.monitoring.gwt.client.ApplicationService;
 import moten.david.util.monitoring.gwt.client.check.AppChecks;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 public class ApplicationServiceImpl extends RemoteServiceServlet implements
-		ApplicationService {
+        ApplicationService {
 
-	private final ApplicationServiceDummy service;
+    private static Logger log = Logger.getLogger(ApplicationServiceImpl.class
+            .getName());
 
-	public ApplicationServiceImpl() {
-		service = new ApplicationServiceDummy();
-	}
+    private static final long serialVersionUID = -7459556404523295829L;
 
-	@Override
-	public void check() {
-		service.check();
+    private ApplicationService service;
 
-	}
+    public ApplicationServiceImpl() {
+        String className = System.getProperty("applicationService");
+        if (className == null) {
+            className = ApplicationServiceDummyProvider.class.getName();
+            log
+                    .info("System property applicationService not set, using dummy provider instead");
+        }
+        log.info("instantiating ApplicationServiceProvider " + className);
+        try {
+            ApplicationServiceProvider provider = (ApplicationServiceProvider) Class
+                    .forName(className).newInstance();
+            service = provider.get();
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	@Override
-	public String getApplicationName() {
-		return service.getApplicationName();
-	}
+    @Override
+    public void check() {
+        service.check();
 
-	@Override
-	public AppChecks getResults() {
-		return service.getResults();
-	}
+    }
+
+    @Override
+    public String getApplicationName() {
+        return service.getApplicationName();
+    }
+
+    @Override
+    public AppChecks getResults() {
+        return service.getResults();
+    }
 
 }
