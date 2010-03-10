548c594e20033414779ca73c46d4e1d183bf49c6
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-03-10 10:07:41 +0000

    use monitor client jar dependency

--- moten-util-parent/trunk/monitor-example/src/main/java/moten/david/util/monitoring/monitor/example/ServiceProvider.java
+++ moten-util-parent/trunk/monitor-example/src/main/java/moten/david/util/monitoring/monitor/example/ServiceProvider.java
@@ -1,5 +1,12 @@
 package moten.david.util.monitoring.monitor.example;
 
-public class ServiceProvider {
+import moten.david.util.monitoring.gwt.client.ApplicationService;
+import moten.david.util.monitoring.gwt.server.ApplicationServiceProvider;
+
+public class ServiceProvider implements ApplicationServiceProvider {
+
+	public ApplicationService get() {
+		return new Service();
+	}
 
 }
