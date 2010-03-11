8bd3bf55a97e2be5b7978c075b0ba4da45ccde15
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-03-11 06:21:14 +0000

    getter method for Checks

--- moten-util-parent/trunk/moten-util/src/main/java/moten/david/util/monitoring/Checker.java
+++ moten-util-parent/trunk/moten-util/src/main/java/moten/david/util/monitoring/Checker.java
@@ -8,58 +8,62 @@ import moten.david.util.monitoring.lookup.LookupType;
 
 public class Checker {
 
-	private final List<Check> checks;
-	private final Level unknown;
-	private final Level ok;
-	private final EvaluationContext expressions;
-	private final Level exception;
+    private final List<Check> checks;
+    private final Level unknown;
+    private final Level ok;
+    private final EvaluationContext expressions;
+    private final Level exception;
 
-	public Checker(EvaluationContext expressions, List<Check> checks, Level ok,
-			Level unknown, Level exception) {
-		this.expressions = expressions;
-		this.checks = checks;
-		this.ok = ok;
-		this.unknown = unknown;
-		this.exception = exception;
-	}
+    public Checker(EvaluationContext expressions, List<Check> checks, Level ok,
+            Level unknown, Level exception) {
+        this.expressions = expressions;
+        this.checks = checks;
+        this.ok = ok;
+        this.unknown = unknown;
+        this.exception = exception;
+    }
 
-	public Map<Check, CheckResult> check() {
-		Map<Check, CheckResult> map = new HashMap<Check, CheckResult>();
-		for (Check check : checks) {
-			check(map, check);
-		}
-		return map;
-	}
+    public Map<Check, CheckResult> check() {
+        Map<Check, CheckResult> map = new HashMap<Check, CheckResult>();
+        for (Check check : checks) {
+            check(map, check);
+        }
+        return map;
+    }
 
-	private CheckResult check(Map<Check, CheckResult> map, Check check) {
+    private CheckResult check(Map<Check, CheckResult> map, Check check) {
 
-		if (map.get(check) == null) {
-			boolean depsOk = true;
-			if (check.getDependencies() != null)
-				for (Dependency dep : check.getDependencies()) {
-					CheckResult result = check(map, dep.getCheck());
-					if (!result.getLevel().equals(ok))
-						depsOk = false;
-				}
-			if (!depsOk)
-				map.put(check, new CheckResult(unknown));
-			else {
-				// set up the lookups
-				MonitoringLookups lookups = expressions.getLookups();
-				for (LookupType type : check.getLookups().keySet())
-					lookups.put(type, check.getLookups().get(type));
-				try {
-					if (check.getExpression().evaluate())
-						map.put(check, new CheckResult(ok));
-					else
-						map
-								.put(check, new CheckResult(check
-										.getFailureLevel()));
-				} catch (RuntimeException e) {
-					map.put(check, new CheckResult(exception, e));
-				}
-			}
-		}
-		return map.get(check);
-	}
+        if (map.get(check) == null) {
+            boolean depsOk = true;
+            if (check.getDependencies() != null)
+                for (Dependency dep : check.getDependencies()) {
+                    CheckResult result = check(map, dep.getCheck());
+                    if (!result.getLevel().equals(ok))
+                        depsOk = false;
+                }
+            if (!depsOk)
+                map.put(check, new CheckResult(unknown));
+            else {
+                // set up the lookups
+                MonitoringLookups lookups = expressions.getLookups();
+                for (LookupType type : check.getLookups().keySet())
+                    lookups.put(type, check.getLookups().get(type));
+                try {
+                    if (check.getExpression().evaluate())
+                        map.put(check, new CheckResult(ok));
+                    else
+                        map
+                                .put(check, new CheckResult(check
+                                        .getFailureLevel()));
+                } catch (RuntimeException e) {
+                    map.put(check, new CheckResult(exception, e));
+                }
+            }
+        }
+        return map.get(check);
+    }
+
+    public List<Check> getChecks() {
+        return checks;
+    }
 }
