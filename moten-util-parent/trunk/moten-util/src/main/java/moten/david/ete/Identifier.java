703af35bac370e10ec45b0b10a8d828460298491
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-03-31 06:08:20 +0000

    more ete dev

--- moten-util-parent/trunk/moten-util/src/main/java/moten/david/ete/Identifier.java
+++ moten-util-parent/trunk/moten-util/src/main/java/moten/david/ete/Identifier.java
@@ -1,17 +1,26 @@
 package moten.david.ete;
 
 /**
- * Identifies an entity.
+ * Identifies an entity. The Comparable interface will be used to sort
+ * Identifiers in ascending order of Identifier Type strength.
  * 
  * @author dave
- * 
  */
-public interface Identifier {
+public abstract class Identifier implements Comparable<Identifier> {
 
-	/**
-	 * Returns the type of the identifier.
-	 * 
-	 * @return
-	 */
-	IdentifierType getIdentifierType();
+    /**
+     * Returns the type of the identifier.
+     * 
+     * @return
+     */
+    public abstract IdentifierType getIdentifierType();
+
+    /*
+     * Use the IdentifierType to do the comparison. (non-Javadoc)
+     * @see java.lang.Comparable#compareTo(java.lang.Object)
+     */
+    @Override
+    public int compareTo(Identifier id) {
+        return this.getIdentifierType().compareTo(id.getIdentifierType());
+    }
 }
