980e05daa5ad4375b5977f4409c181344a75f826
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-04-08 00:44:44 +0000

    tests

--- moten-util-parent/trunk/moten-util/src/main/java/moten/david/imatch/memory/DatastoreImmutable.java
+++ moten-util-parent/trunk/moten-util/src/main/java/moten/david/imatch/memory/DatastoreImmutable.java
@@ -20,135 +20,134 @@ import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
-import com.google.common.collect.ImmutableMap.Builder;
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
 
 public class DatastoreImmutable extends DatastoreBase {
 
-	private final ImmutableMap<Identifier, IdentifierSet> map;
-	private final ImmutableMap<IdentifierSet, Double> times;
-	private final IdentifierSetFactory identifierSetFactory;
-	private final IdentifierTypeSetFactory identifierTypeSetFactory;
-	private final IdentifierComparator identifierComparator;
-	private final IdentifierTypeStrengthComparator identifierTypeStrengthComparator;
+    private final ImmutableMap<Identifier, IdentifierSet> map;
+    private final ImmutableMap<IdentifierSet, Double> times;
+    private final IdentifierSetFactory identifierSetFactory;
+    private final IdentifierTypeSetFactory identifierTypeSetFactory;
+    private final IdentifierComparator identifierComparator;
+    private final IdentifierTypeStrengthComparator identifierTypeStrengthComparator;
 
-	@Inject
-	public DatastoreImmutable(IdentifierSetFactory identifierSetFactory,
-			IdentifierTypeSetFactory identifierTypeSetFactory,
-			IdentifierComparator identifierComparator,
-			IdentifierTypeStrengthComparator identifierTypeStrengthComparator,
-			@Assisted Map<Identifier, IdentifierSet> map,
-			@Assisted Map<IdentifierSet, Double> times) {
-		super(identifierSetFactory, identifierTypeSetFactory);
-		this.identifierSetFactory = identifierSetFactory;
-		this.identifierTypeSetFactory = identifierTypeSetFactory;
-		this.identifierComparator = identifierComparator;
-		this.identifierTypeStrengthComparator = identifierTypeStrengthComparator;
-		this.map = ImmutableMap.copyOf(map);
-		this.times = ImmutableMap.copyOf(times);
-	}
+    @Inject
+    public DatastoreImmutable(IdentifierSetFactory identifierSetFactory,
+            IdentifierTypeSetFactory identifierTypeSetFactory,
+            IdentifierComparator identifierComparator,
+            IdentifierTypeStrengthComparator identifierTypeStrengthComparator,
+            @Assisted Map<Identifier, IdentifierSet> map,
+            @Assisted Map<IdentifierSet, Double> times) {
+        super(identifierSetFactory, identifierTypeSetFactory);
+        this.identifierSetFactory = identifierSetFactory;
+        this.identifierTypeSetFactory = identifierTypeSetFactory;
+        this.identifierComparator = identifierComparator;
+        this.identifierTypeStrengthComparator = identifierTypeStrengthComparator;
+        this.map = ImmutableMap.copyOf(map);
+        this.times = ImmutableMap.copyOf(times);
+    }
 
-	@Override
-	public IdentifierSet alpha(Identifier identifier) {
-		IdentifierSet value = map.get(identifier);
-		if (value == null)
-			return MyIdentifierSet.EMPTY_SET;
-		else
-			return value;
-	}
+    @Override
+    public IdentifierSet alpha(Identifier identifier) {
+        IdentifierSet value = map.get(identifier);
+        if (value == null)
+            return MyIdentifierSet.EMPTY_SET;
+        else
+            return value;
+    }
 
-	@Override
-	public double time(IdentifierSet set) {
-		if (times.get(set) == null)
-			return Double.MIN_NORMAL;
-		else
-			return times.get(set);
-	}
+    @Override
+    public double time(IdentifierSet set) {
+        if (times.get(set) == null)
+            return Double.MIN_NORMAL;
+        else
+            return times.get(set);
+    }
 
-	@Override
-	public ImmutableSet<Identifier> identifiers() {
-		return map.keySet();
-	}
+    @Override
+    public ImmutableSet<Identifier> identifiers() {
+        return map.keySet();
+    }
 
-	@Override
-	public Datastore add(IdentifierSet set, double time) {
-		Builder<Identifier, IdentifierSet> mapBuilder = ImmutableMap.builder();
-		HashMap<IdentifierSet, Double> newTimes = new HashMap<IdentifierSet, Double>();
-		for (Identifier i : Sets.union(map.keySet(), set.set())) {
-			IdentifierSet s = merge(set, i);
-			mapBuilder.put(i, s);
-			if (set.contains(i))
-				newTimes.put(s, time);
-			else
-				newTimes.put(s, times.get(s));
-		}
-		return new DatastoreImmutable(identifierSetFactory,
-				identifierTypeSetFactory, identifierComparator,
-				identifierTypeStrengthComparator, mapBuilder.build(), newTimes);
-	}
+    @Override
+    public Datastore add(IdentifierSet set, double time) {
+        HashMap<Identifier, IdentifierSet> newMap = new HashMap<Identifier, IdentifierSet>();
+        HashMap<IdentifierSet, Double> newTimes = new HashMap<IdentifierSet, Double>();
+        for (Identifier i : Sets.union(map.keySet(), set.set())) {
+            IdentifierSet s = merge(set, i);
+            newMap.put(i, s);
+            if (set.contains(i))
+                newTimes.put(s, time);
+            else
+                newTimes.put(s, times.get(s));
+        }
+        return new DatastoreImmutable(identifierSetFactory,
+                identifierTypeSetFactory, identifierComparator,
+                identifierTypeStrengthComparator, newMap, newTimes);
+    }
 
-	@Override
-	public double d(IdentifierType t) {
-		return t.getOrder();
-	}
+    @Override
+    public double d(IdentifierType t) {
+        return t.getOrder();
+    }
 
-	@Override
-	public double dmax(IdentifierSet s) {
-		if (s.set().size() == 0)
-			return Double.MIN_VALUE;
-		else {
-			Identifier idmax = Collections.max(s.set(), identifierComparator);
-			return idmax.getIdentifierType().getOrder();
-		}
-	}
+    @Override
+    public double dmax(IdentifierSet s) {
+        if (s.set().size() == 0)
+            return Double.MIN_VALUE;
+        else {
+            Identifier idmax = Collections.max(s.set(), identifierComparator);
+            return idmax.getIdentifierType().getOrder();
+        }
+    }
 
-	@Override
-	public IdentifierSet nms(IdentifierSet x, final IdentifierSet y) {
-		return x.filter(new Predicate<Identifier>() {
-			@Override
-			public boolean apply(Identifier i) {
-				return !y.contains(i)
-						&& i.getIdentifierType().getStrength() > Collections
-								.max(y.types().set(),
-										identifierTypeStrengthComparator)
-								.getStrength();
-			}
-		});
-	}
+    @Override
+    public IdentifierSet nms(IdentifierSet x, final IdentifierSet y) {
+        return x.filter(new Predicate<Identifier>() {
+            @Override
+            public boolean apply(Identifier i) {
+                return !y.contains(i)
+                        && i.getIdentifierType().getStrength() > Collections
+                                .max(y.types().set(),
+                                        identifierTypeStrengthComparator)
+                                .getStrength();
+            }
+        });
+    }
 
-	@Override
-	public IdentifierSet pm(IdentifierSet ids) {
-		Set<IdentifierSet> alphaX = Functional.apply(ids.set(),
-				new Function<Identifier, IdentifierSet>() {
-					@Override
-					public IdentifierSet apply(Identifier s) {
-						return alpha(s);
-					}
-				});
-		IdentifierSet y = Collections.max(alphaX,
-				new Comparator<IdentifierSet>() {
-					@Override
-					public int compare(IdentifierSet o1, IdentifierSet o2) {
-						return Double.compare(dmax(o1), dmax(o2));
-					}
-				});
-		return y;
-	}
+    @Override
+    public IdentifierSet pm(IdentifierSet ids) {
+        Set<IdentifierSet> alphaX = Functional.apply(ids.set(),
+                new Function<Identifier, IdentifierSet>() {
+                    @Override
+                    public IdentifierSet apply(Identifier s) {
+                        return alpha(s);
+                    }
+                });
+        IdentifierSet y = Collections.max(alphaX,
+                new Comparator<IdentifierSet>() {
+                    @Override
+                    public int compare(IdentifierSet o1, IdentifierSet o2) {
+                        return Double.compare(dmax(o1), dmax(o2));
+                    }
+                });
+        return y;
+    }
 
-	@Override
-	public IdentifierType t(Identifier x) {
-		return x.getIdentifierType();
-	}
+    @Override
+    public IdentifierType t(Identifier x) {
+        return x.getIdentifierType();
+    }
 
-	@Override
-	public String toString() {
-		StringBuffer s = new StringBuffer();
-		s.append("DataStoreImmutable=\n");
-		for (Identifier i : map.keySet())
-			s.append(i + " => " + map.get(i) + "\n");
-		return s.toString();
+    @Override
+    public String toString() {
+        StringBuffer s = new StringBuffer();
+        s.append("DataStoreImmutable=\n");
+        for (Identifier i : map.keySet())
+            s.append(i + " => " + map.get(i) + "\n");
+        return s.toString();
 
-	}
+    }
 
 }
--- moten-util-parent/trunk/moten-util/src/test/java/moten/david/imatch/memory/DatastoreImmutableTest.java
+++ moten-util-parent/trunk/moten-util/src/test/java/moten/david/imatch/memory/DatastoreImmutableTest.java
@@ -17,65 +17,70 @@ import com.google.inject.Injector;
 
 public class DatastoreImmutableTest {
 
-	private long millis = System.currentTimeMillis();
+    private long millis = System.currentTimeMillis();
 
-	private static Logger log = Logger.getLogger(DatastoreImmutableTest.class
-			.getName());
+    private static Logger log = Logger.getLogger(DatastoreImmutableTest.class
+            .getName());
 
-	private final Injector injector = Guice
-			.createInjector(new InjectorModule());
+    private final Injector injector = Guice
+            .createInjector(new InjectorModule());
 
-	@Test
-	public void test() {
+    @Test
+    public void test() {
 
-		DatastoreImmutableFactory factory = injector
-				.getInstance(DatastoreImmutableFactory.class);
+        DatastoreImmutableFactory factory = injector
+                .getInstance(DatastoreImmutableFactory.class);
 
-		Datastore ds = factory.create(ImmutableMap
-				.copyOf(new HashMap<Identifier, IdentifierSet>()), ImmutableMap
-				.copyOf(new HashMap<IdentifierSet, Double>()));
+        Datastore ds = factory.create(ImmutableMap
+                .copyOf(new HashMap<Identifier, IdentifierSet>()), ImmutableMap
+                .copyOf(new HashMap<IdentifierSet, Double>()));
 
-		IdentifierSet ids = createSet();
-		{
-			ids = ids.add(createIdentifier("name1", "fred", 10, 10));
-			ds = ds.add(ids, millis++);
-			Assert.assertEquals(1, ds.identifiers().size());
+        IdentifierSet ids = createSet();
+        {
+            ids = ids.add(createIdentifier("name1", "fred", 10, 10));
+            ds = add(ds, ids);
+            Assert.assertEquals(1, ds.identifiers().size());
 
-			// no change on readding it
-			ds = ds.add(ids, millis++);
-			Assert.assertEquals(1, ds.identifiers().size());
+            // no change on readding it
+            ds = add(ds, ids);
+            Assert.assertEquals(1, ds.identifiers().size());
 
-			ids = createSet();
-			ids = ids.add(createIdentifier("name2", "joe", 10, 11));
-			ds = ds.add(ids, millis++);
-			Assert.assertEquals(2, ds.identifiers().size());
+            ids = createSet();
+            ids = ids.add(createIdentifier("name2", "joe", 10, 11));
+            ds = add(ds, ids);
+            Assert.assertEquals(2, ds.identifiers().size());
 
-			ids = createSet();
-			ids = ids.add(createIdentifier("name2", "keith", 10, 11));
-			ds = ds.add(ids, millis++);
-			Assert.assertEquals(3, ds.identifiers().size());
+            ids = createSet();
+            ids = ids.add(createIdentifier("name2", "keith", 10, 11));
+            ds = add(ds, ids);
+            Assert.assertEquals(3, ds.identifiers().size());
 
-			ids = createSet();
-			ids = ids.add(createIdentifier("name2", "keith", 10, 11));
-			ids = ids.add(createIdentifier("name3", "john", 10, 12));
-			ds = ds.add(ids, millis++);
-			log.info(ds.toString());
-			// Assert.assertEquals(3, ds.identifiers().size());
+            ids = createSet();
+            ids = ids.add(createIdentifier("name2", "keith", 10, 11));
+            ids = ids.add(createIdentifier("name3", "john", 10, 12));
+            ds = add(ds, ids);
+            // Assert.assertEquals(3, ds.identifiers().size());
 
-		}
+        }
 
-	}
+    }
 
-	private Identifier createIdentifier(String name, String value,
-			int strength, int order) {
-		MyIdentifierType type = new MyIdentifierType(name, strength, order);
-		return new MyIdentifier(type, value);
-	}
+    public Datastore add(final Datastore ds, final IdentifierSet ids) {
+        Datastore ds2 = ds.add(ids, millis++);
+        log.info(ds2.toString());
+        return ds2;
+    }
 
-	private IdentifierSet createSet() {
-		IdentifierSetFactory idSetFactory = injector
-				.getInstance(IdentifierSetFactory.class);
-		IdentifierSet ids = idSetFactory.create();
-		return ids;
-	}
+    private Identifier createIdentifier(String name, String value,
+            int strength, int order) {
+        MyIdentifierType type = new MyIdentifierType(name, strength, order);
+        return new MyIdentifier(type, value);
+    }
+
+    private IdentifierSet createSet() {
+        IdentifierSetFactory idSetFactory = injector
+                .getInstance(IdentifierSetFactory.class);
+        IdentifierSet ids = idSetFactory.create();
+        return ids;
+    }
 }
