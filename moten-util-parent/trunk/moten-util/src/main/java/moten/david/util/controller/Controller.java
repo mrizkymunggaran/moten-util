87e6b1c4c479c7ca4fb6b6a7704d8a61a5bfb347
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-04-19 21:09:20 +0000

    added remove method and javadoc

--- moten-util-parent/trunk/moten-util/src/main/java/moten/david/util/controller/Controller.java
+++ moten-util-parent/trunk/moten-util/src/main/java/moten/david/util/controller/Controller.java
@@ -7,11 +7,22 @@ import java.util.Map;
 
 import com.google.inject.Singleton;
 
+/**
+ * Acts as an event bus. Notifies registered listeners of events.
+ * 
+ * @author Dave Moten
+ * 
+ */
 @Singleton
 public class Controller {
-
 	private final Map<Class<? extends Event>, List<ControllerListener<? extends Event>>> map = new HashMap<Class<? extends Event>, List<ControllerListener<? extends Event>>>();
 
+	/**
+	 * Notifies the controller that an event occurred. The controller will then
+	 * notify registered listeners interested in that type of event.
+	 * 
+	 * @param event
+	 */
 	public void event(Event event) {
 		for (Class<? extends Event> cls : map.keySet()) {
 			if (cls.isInstance(event)) {
@@ -22,10 +33,35 @@ public class Controller {
 		}
 	}
 
-	public <T extends Event> void addListener(Class<T> cls,
+	/**
+	 * Adds a listener for events of type cls. Also will be notified of any
+	 * events that are subclasses of cls.
+	 * 
+	 * @param <T>
+	 * @param cls
+	 * @param listener
+	 */
+	public synchronized <T extends Event> void addListener(Class<T> cls,
 			ControllerListener<T> listener) {
 		if (map.get(cls) == null)
 			map.put(cls, new ArrayList<ControllerListener<? extends Event>>());
 		map.get(cls).add(listener);
+	}
+
+	/**
+	 * Returns true if and only if the listener was registered with the
+	 * controlller and was successfully removed.
+	 * 
+	 * @param <T>
+	 * @param cls
+	 * @param listener
+	 * @return
+	 */
+	public synchronized <T extends Event> boolean removeListener(Class<T> cls,
+			ControllerListener<T> listener) {
+		if (map.get(cls) == null)
+			return false;
+		else
+			return map.get(cls).remove(listener);
 	}
 }
