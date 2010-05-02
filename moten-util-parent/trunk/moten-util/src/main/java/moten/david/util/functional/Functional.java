92e8c1ac7b7be155e540f4f2d07a50634ae956a2
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-05-02 22:57:07 +0000

    concurrent implementations of Functional.apply and Functional.filter

--- moten-util-parent/trunk/moten-util/src/main/java/moten/david/util/functional/Functional.java
+++ moten-util-parent/trunk/moten-util/src/main/java/moten/david/util/functional/Functional.java
@@ -1,8 +1,13 @@
 package moten.david.util.functional;
 
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSet.Builder;
 
@@ -27,6 +32,64 @@ public class Functional {
 		for (T s : set)
 			if (predicate.apply(s))
 				builder.add(s);
+		return builder.build();
+	}
+
+	public static <S, T> Set<T> apply(Set<S> set, final Function<S, T> f,
+			final ExecutorService executorService) {
+		com.google.common.collect.ImmutableList.Builder<Future<T>> futures = ImmutableList
+				.builder();
+		for (final S s : set)
+			futures.add(executorService.submit(new Callable<T>() {
+				@Override
+				public T call() throws Exception {
+					return f.apply(s);
+				}
+			}));
+
+		Builder<T> builder = ImmutableSet.builder();
+		for (Future<T> future : futures.build()) {
+			T result;
+			try {
+				result = future.get();
+				builder.add(result);
+			} catch (InterruptedException e) {
+				// do nothing
+			} catch (ExecutionException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return builder.build();
+	}
+
+	public static <T> Set<T> filter(Set<T> set, final Predicate<T> predicate,
+			ExecutorService executorService) {
+		com.google.common.collect.ImmutableList.Builder<Future<T>> futures = ImmutableList
+				.builder();
+		for (final T s : set)
+			futures.add(executorService.submit(new Callable<T>() {
+				@Override
+				public T call() throws Exception {
+					if (predicate.apply(s))
+						return s;
+					else
+						return null;
+				}
+			}));
+
+		Builder<T> builder = ImmutableSet.builder();
+		for (Future<T> future : futures.build()) {
+			T result;
+			try {
+				result = future.get();
+				if (result != null)
+					builder.add(result);
+			} catch (InterruptedException e) {
+				// do nothing
+			} catch (ExecutionException e) {
+				throw new RuntimeException(e);
+			}
+		}
 		return builder.build();
 	}
 }
