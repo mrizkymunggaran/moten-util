5b42de9f70597915965b898080ee8276e931c426
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-05-10 03:28:00 +0000

    squabble early dev

--- squabble/trunk/squabble-core/src/main/java/moten/david/squabble/Controller.java
+++ squabble/trunk/squabble-core/src/main/java/moten/david/squabble/Controller.java
@@ -7,6 +7,7 @@ import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableListMultimap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSortedSet;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Sets;
 import com.google.common.collect.ImmutableList.Builder;
@@ -17,24 +18,62 @@ public class Controller {
 			.of();
 
 	private ImmutableListMultimap<String, String> add(String user, String word) {
-		List<String> words = createWordFrom(getCurrentWords(), word);
+		Iterable<String> words = createWordFrom(getCurrentWords(), word);
 	}
 
-	private List<String> createWordFrom(List<String> currentWords,
+	private Iterable<String> createWordFrom(List<String> currentWords,
 			final String word) {
-		Set<Character> wordSet = getSet(word);
+		final Set<Character> wordSet = getSet(word);
 		Iterable<String> intersects = Iterables.filter(currentWords,
 				new Predicate<String>() {
 					@Override
 					public boolean apply(String w) {
-						return Sets.intersection(getSet(w), wordSet).size() > 0;
+						return w.length() <= word.length()
+								&& Sets.intersection(getSet(w), wordSet).size() > 0;
 					}
-
 				});
 
+		return intersects;
+	}
+
+	private Iterable<String> createWordFrom(Iterable<String> used,
+			Iterable<String> unused, final String word) {
+		String usedJoined = getSingledWord(used);
+		if (usedJoined.length() > word.length())
+			return null;
+		else {
+			if (getSet(usedJoined).equals(usedJoined))
+		}
+	}
+
+	private String getSingledWord(Iterable<String> strings) {
+		StringBuffer s = new StringBuffer();
+		for (String str : strings)
+			s.append(str);
+		return s.toString();
+	}
+
+	private String sort(String word) {
+		com.google.common.collect.ImmutableSortedSet.Builder<Character> builder = ImmutableSortedSet
+				.naturalOrder();
+		for (Character ch : word.toCharArray())
+			builder.add(ch);
+		StringBuffer s = new StringBuffer();
+		for (Character ch : builder.build()) {
+			s.append(ch);
+		}
+		return s.toString();
 	}
 
 	private Set<Character> getSet(String word) {
+		com.google.common.collect.ImmutableSet.Builder<Character> builder = ImmutableSet
+				.builder();
+		for (Character ch : word.toCharArray())
+			builder.add(ch);
+		return builder.build();
+	}
+
+	private Set<Character> getList(String word) {
 		com.google.common.collect.ImmutableSet.Builder<Character> builder = ImmutableSet
 				.builder();
 		for (Character ch : word.toCharArray())
