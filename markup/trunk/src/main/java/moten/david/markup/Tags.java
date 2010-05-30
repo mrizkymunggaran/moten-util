42c4aa15a05f58701209232d08a2715ee61723cf
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-05-30 12:33:06 +0000

    more marker dev

--- markup/trunk/src/main/java/moten/david/markup/Tags.java
+++ markup/trunk/src/main/java/moten/david/markup/Tags.java
@@ -23,14 +23,29 @@ public class Tags {
 			throw new RuntimeException(e);
 		}
 		tags = new ArrayList<Tag>();
+		List<String> names = new ArrayList<String>();
 		for (String line : list) {
 			line = line.trim();
 			if (line.length() > 0 && !line.startsWith("#")) {
 				String[] items = line.split("\t");
 				String name = items[0];
 				Color colour = Color.decode("0x" + items[1]);
+
 				tags.add(new Tag(name, colour));
+				names.add(name);
 			}
+		}
+		tags.clear();
+		float s = 1;
+		float b = 240f / 255;
+		float h = 0;
+		float step = 1f / names.size();
+
+		for (String name : names) {
+			h += step;
+			System.out.println("h=" + h);
+			Color colour = new Color(Color.HSBtoRGB(h, s, b));
+			tags.add(new Tag(name, colour));
 		}
 
 	}
