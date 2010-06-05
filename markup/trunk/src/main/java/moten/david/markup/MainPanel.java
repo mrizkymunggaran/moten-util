f4ba61cab55f24b51c7bd8cd96f6b14d9ece9ba4
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-06-05 09:37:07 +0000

    rotation of stripe

--- markup/trunk/src/main/java/moten/david/markup/MainPanel.java
+++ markup/trunk/src/main/java/moten/david/markup/MainPanel.java
@@ -6,6 +6,7 @@ import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.Rectangle;
+import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -99,7 +100,11 @@ public class MainPanel extends JPanel {
 			protected void paintComponent(Graphics g) {
 				synchronized (study) {
 					super.paintComponent(g);
+
 					Graphics2D g2d = (Graphics2D) g;
+					g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+							RenderingHints.VALUE_ANTIALIAS_ON);
+
 					controller.event(new StartingToPaintTags());
 					try {
 						for (DocumentTag documentTag : document
