c3eefa835a588d1fb58b1a6ecfb44b3c321be3a3
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-06-06 09:41:45 +0000

    separate graphics instance

--- markup/trunk/src/test/java/moten/david/markup/StudyMarshallerTest.java
+++ markup/trunk/src/test/java/moten/david/markup/StudyMarshallerTest.java
@@ -9,22 +9,22 @@ import org.junit.Test;
 
 public class StudyMarshallerTest {
 
-	@Test
-	public void testMarshalling() {
-		StudyMarshaller m = new StudyMarshaller();
-		InputStream is = getClass().getResourceAsStream(
-				"/study1/markup/study.xml");
-		Study study = m.unmarshal(is);
-		Assert.assertEquals("Pro Cycling Interview", study.getName());
-		Assert.assertEquals(1, study.getDocument().size());
+    @Test
+    public void testMarshalling() {
+        StudyMarshaller m = new StudyMarshaller();
+        InputStream is = getClass().getResourceAsStream(
+                "/study1/markup/study.xml");
+        Study study = m.unmarshal(is);
+        Assert.assertEquals("Pro Cycling Interview", study.getName());
+        Assert.assertTrue(study.getDocument().size() > 0);
 
-		m.marshal(study, System.out);
-	}
+        m.marshal(study, System.out);
+    }
 
-	@Test
-	public void testRegex() {
-		String s = "PRDN:=AusSAR:190";
-		s = s.replaceFirst("^PRDN:=(.*)", "PRDN:INMARSATC=$1");
-		Assert.assertEquals("replaced address", "PRDN:INMARSATC=AusSAR:190", s);
-	}
+    @Test
+    public void testRegex() {
+        String s = "PRDN:=AusSAR:190";
+        s = s.replaceFirst("^PRDN:=(.*)", "PRDN:INMARSATC=$1");
+        Assert.assertEquals("replaced address", "PRDN:INMARSATC=AusSAR:190", s);
+    }
 }
