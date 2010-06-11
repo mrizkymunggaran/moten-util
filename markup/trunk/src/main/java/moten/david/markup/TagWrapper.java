ac645e3fa63d83513771037646df9515b20844a6
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-06-11 23:53:41 +0000

    markup: use inheritance in schema to model logical tags

--- markup/trunk/src/main/java/moten/david/markup/TagWrapper.java
+++ markup/trunk/src/main/java/moten/david/markup/TagWrapper.java
@@ -1,5 +1,6 @@
 package moten.david.markup;
 
+import moten.david.markup.xml.study.SimpleTag;
 import moten.david.markup.xml.study.Tag;
 
 /**
@@ -19,14 +20,17 @@ public class TagWrapper {
 	@Override
 	public String toString() {
 		StringBuffer s = new StringBuffer();
-		for (Integer tagId : tag.getMemberOf()) {
-			if (s.length() > 0)
-				s.append(",");
-			s.append(tagId);
-		}
-		if (s.length() > 0) {
-			s.insert(0, "(");
-			s.append(")");
+		if (tag instanceof SimpleTag) {
+			SimpleTag t = (SimpleTag) tag;
+			for (Integer tagId : t.getMemberOf()) {
+				if (s.length() > 0)
+					s.append(",");
+				s.append(tagId);
+			}
+			if (s.length() > 0) {
+				s.insert(0, "(");
+				s.append(")");
+			}
 		}
 		return tag.getId() + ". " + tag.getName() + " " + s;
 	}
