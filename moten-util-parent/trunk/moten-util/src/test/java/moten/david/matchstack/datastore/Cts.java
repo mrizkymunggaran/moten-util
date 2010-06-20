817193b8a5f057b78f8c8e590aeb13f7506d1384
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-06-20 22:36:41 +0000

    store position as ancillary data in Cts datastore

--- moten-util-parent/trunk/moten-util/src/test/java/moten/david/matchstack/datastore/Cts.java
+++ moten-util-parent/trunk/moten-util/src/test/java/moten/david/matchstack/datastore/Cts.java
@@ -1,5 +1,6 @@
 package moten.david.matchstack.datastore;
 
+import java.awt.geom.Point2D;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -72,11 +73,15 @@ public class Cts {
             long count = 0;
             long start = System.currentTimeMillis();
             System.out.println(new Date());
+            Point2D.Double pt = null;
             while ((line = br.readLine()) != null) {
                 String[] items = line.split("\t");
                 String fixId = items[0];
                 String name = items[1];
                 String value = items[2];
+                double lat = Double.parseDouble(items[3]);
+                double lon = Double.parseDouble(items[4]);
+                pt = new Point2D.Double(lat, lon);
                 long time = df.parse(items[3]).getTime();
                 if (name.equals("IMO Number") && value.length() == 7) {
                     // System.out.println(fixId + "," + name + "," + value + ","
@@ -87,7 +92,7 @@ public class Cts {
                     count++;
                     if (lastFixId != null && !fixId.equals(lastFixId)) {
                         ImmutableSet<TimedIdentifier> set = builder.build();
-                        ds = ds.add(set, null);
+                        ds = ds.add(set, pt);
                         builder = ImmutableSet.builder();
                     }
                     builder.add(ti);
@@ -99,7 +104,7 @@ public class Cts {
                     }
                 }
             }
-            ds.add(builder.build(), null);
+            ds.add(builder.build(), pt);
             // System.out.println(ds.toString());
             System.out.println("sets =" + ds.sets().size());
             System.out.println(count);
