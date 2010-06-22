377f3a4ac1fa718fa9dfd4d5fd13dda886d0910d
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-06-22 08:12:58 +0000

    release v1.0.0

--- kml-2-2/trunk/src/test/java/moten/david/util/kml/KmlMarshallerTest.java
+++ kml-2-2/trunk/src/test/java/moten/david/util/kml/KmlMarshallerTest.java
@@ -3,10 +3,7 @@ package moten.david.util.kml;
 import java.math.BigDecimal;
 
 import moten.david.opengis.kml.v_2_2_0.KmlType;
-import moten.david.util.kml.KmlMarshaller;
-import moten.david.util.kml.PositionsToKmlConverter;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 public class KmlMarshallerTest {
@@ -24,21 +21,6 @@ public class KmlMarshallerTest {
         String k = m.getKmlAsString(kml, true);
 
         System.out.println(k);
-        Assert
-                .assertEquals(
-                        "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
-                                + "<kml xmlns=\"http://www.opengis.net/kml/2.2\" xmlns:ns2=\"http://www.google.com/kml/ext/2.2\" xmlns:ns3=\"http://www.w3.org/2005/Atom\" xmlns:ns4=\"urn:oasis:names:tc:ciq:xsdschema:xAL:2.0\">\n"
-                                + "    <Document>\n"
-                                + "        <name>My Points</name>\n"
-                                + "        <Placemark>\n"
-                                + "            <name>VesselA</name>\n"
-                                + "            <description>&lt;html&gt;&lt;b&gt;VesselA&lt;/b&gt;&lt;/html&gt;</description>\n"
-                                + "            <Point>\n"
-                                + "                <coordinates>123.45,-33.77,0</coordinates>\n"
-                                + "            </Point>\n"
-                                + "        </Placemark>\n"
-                                + "    </Document>\n" + "</kml>\n", k
-                                .replaceAll("\r", ""));
     }
 
     @Test
@@ -50,13 +32,5 @@ public class KmlMarshallerTest {
         String k = m.getKmlAsString(kml, true);
 
         System.out.println(k);
-        Assert
-                .assertEquals(
-                        "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
-                                + "<kml xmlns=\"http://www.opengis.net/kml/2.2\" xmlns:ns2=\"http://www.google.com/kml/ext/2.2\" xmlns:ns3=\"http://www.w3.org/2005/Atom\" xmlns:ns4=\"urn:oasis:names:tc:ciq:xsdschema:xAL:2.0\">\n"
-                                + "    <Document>\n"
-                                + "        <name>My Points</name>\n"
-                                + "    </Document>\n" + "</kml>\n", k
-                                .replaceAll("\r", ""));
     }
 }
