75f16ee988bb0b75b439f9e8134ff3f900bb74ff
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-06-23 08:06:20 +0000

    checking in to save dev

--- moten-util-parent/trunk/moten-util/src/main/java/moten/david/matchstack/types/impl/MyIdentifier.java
+++ moten-util-parent/trunk/moten-util/src/main/java/moten/david/matchstack/types/impl/MyIdentifier.java
@@ -3,71 +3,72 @@ package moten.david.matchstack.types.impl;
 import moten.david.matchstack.types.Identifier;
 import moten.david.matchstack.types.IdentifierType;
 
+import com.google.inject.internal.Preconditions;
+
 /**
  * Implementation of an {@link Identifier}.
  * 
  * @author dave
- * 
  */
 public class MyIdentifier implements Identifier {
 
-	private final MyIdentifierType type;
-	private final String value;
+    private final MyIdentifierType type;
+    private final String value;
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param type
-	 * @param value
-	 */
-	public MyIdentifier(MyIdentifierType type, String value) {
-		super();
-		this.type = type;
-		this.value = value;
-	}
+    /**
+     * Constructor.
+     * 
+     * @param type
+     * @param value
+     */
+    public MyIdentifier(MyIdentifierType type, String value) {
+        super();
+        this.type = Preconditions.checkNotNull(type);
+        this.value = value;
+    }
 
-	@Override
-	public IdentifierType getIdentifierType() {
-		return type;
-	}
+    @Override
+    public IdentifierType getIdentifierType() {
+        return type;
+    }
 
-	@Override
-	public String toString() {
-		return type + ":" + value;
-	}
+    @Override
+    public String toString() {
+        return type + ":" + value;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((type == null) ? 0 : type.hashCode());
-		result = prime * result + ((value == null) ? 0 : value.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		MyIdentifier other = (MyIdentifier) obj;
-		if (type == null) {
-			if (other.type != null)
-				return false;
-		} else if (!type.equals(other.type))
-			return false;
-		if (value == null) {
-			if (other.value != null)
-				return false;
-		} else if (!value.equals(other.value))
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        MyIdentifier other = (MyIdentifier) obj;
+        if (type == null) {
+            if (other.type != null)
+                return false;
+        } else if (!type.equals(other.type))
+            return false;
+        if (value == null) {
+            if (other.value != null)
+                return false;
+        } else if (!value.equals(other.value))
+            return false;
+        return true;
+    }
 
-	public String getValue() {
-		return value;
-	}
+    public String getValue() {
+        return value;
+    }
 }
--- moten-util-parent/trunk/moten-util/src/test/java/moten/david/matchstack/datastore/Cts.java
+++ moten-util-parent/trunk/moten-util/src/test/java/moten/david/matchstack/datastore/Cts.java
@@ -24,6 +24,7 @@ import moten.david.matchstack.types.impl.MyTimedIdentifier;
 import moten.david.util.kml.KmlMarshaller;
 import moten.david.util.kml.PositionsToKmlConverter;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSetMultimap;
 import com.google.common.collect.ImmutableSet.Builder;
@@ -33,141 +34,145 @@ import com.google.inject.Injector;
 
 public class Cts {
 
-	private static DateFormat df = new SimpleDateFormat(
-			"dd/MM/yyyy h:mm:ss.SSSSSS a");
-	@Inject
-	private DatastoreImmutableFactory factory;
+    private static DateFormat df = new SimpleDateFormat(
+            "dd/MM/yyyy h:mm:ss.SSSSSS a");
+    @Inject
+    private DatastoreImmutableFactory factory;
 
-	public void init() {
-		Injector injector = Guice.createInjector(new InjectorModule());
-		injector.injectMembers(this);
-	}
+    public void init() {
+        Injector injector = Guice.createInjector(new InjectorModule());
+        injector.injectMembers(this);
+    }
 
-	private DatastoreImmutable createDatastore() {
-		ImmutableSet<Set<TimedIdentifier>> a = ImmutableSet.of();
-		DatastoreImmutable d = factory.create(a,
-				new HashMap<Identifier, Object>());
-		return d;
-	}
+    private DatastoreImmutable createDatastore() {
+        ImmutableSet<Set<TimedIdentifier>> a = ImmutableSet.of();
+        DatastoreImmutable d = factory.create(a,
+                new HashMap<Identifier, Object>());
+        return d;
+    }
 
-	public void load(InputStream is) {
-		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-		String line;
+    public void load(InputStream is) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String line;
 
-		init();
-		DatastoreImmutable ds = createDatastore();
-		Map<String, Double> strengths = new HashMap<String, Double>() {
-			{
-				put("MMSI", 2.0);
-				put("IMO Number", 3.0);
-				put("Callsign", 1.0);
-				put("Inmarsat C Mobile Number", 1.5);
-				put("Terminal ID", 1.5);
-			}
-		};
-		Map<String, MyIdentifierType> types = new HashMap<String, MyIdentifierType>();
-		for (String name : strengths.keySet())
-			types.put(name, new MyIdentifierType(name, strengths.get(name)));
+        init();
+        DatastoreImmutable ds = createDatastore();
+        Map<String, Double> strengths = new HashMap<String, Double>() {
+            {
+                put("MMSI", 2.0);
+                put("IMO Number", 3.0);
+                put("Callsign", 1.0);
+                put("Inmarsat C Mobile Number", 1.5);
+                put("Terminal ID", 1.5);
+                put("Serial Number", 1.5);
+            }
+        };
+        Map<String, MyIdentifierType> types = new HashMap<String, MyIdentifierType>();
+        for (String name : strengths.keySet())
+            types.put(name, new MyIdentifierType(name, strengths.get(name)));
 
-		try {
-			// skip header line
-			br.readLine();
-			String lastFixId = null;
-			Builder<TimedIdentifier> builder = ImmutableSet.builder();
-			long count = 0;
-			long start = System.currentTimeMillis();
-			System.out.println(new Date());
-			Info info = null;
-			while ((line = br.readLine()) != null) {
-				String[] items = line.split("\t");
-				String fixId = items[0];
-				String name = items[1];
-				String value = items[2];
-				info = new Info();
-				info.time = df.parse(items[3]).getTime();
-				info.lat = new BigDecimal(items[4]);
-				info.lon = new BigDecimal(items[5]);
-				if (!name.equals("IMO Number")
-						|| (name.equals("IMO Number") && value.length() == 7)) {
-					// System.out.println(fixId + "," + name + "," + value + ","
-					// + new Date(time));
-					MyIdentifierType type = types.get(name);
-					MyIdentifier id = new MyIdentifier(type, value);
-					MyTimedIdentifier ti = new MyTimedIdentifier(id, info.time);
-					count++;
-					if (lastFixId != null && !fixId.equals(lastFixId)) {
-						ImmutableSet<TimedIdentifier> set = builder.build();
-						ds = ds.add(set, info);
-						builder = ImmutableSet.builder();
-					}
-					builder.add(ti);
-					lastFixId = fixId;
-					if (count % 5000 == 0) {
-						System.out.println("sets =" + ds.sets().size());
-						System.out.println(count);
-					}
-				}
-			}
-			ds.add(builder.build(), info);
-			System.out.println("sets =" + ds.sets().size());
-			System.out.println(count);
-			long durationMs = System.currentTimeMillis() - start;
-			System.out.println(durationMs);
-			System.out.println(count / 1.0 / durationMs * 1000);
-			com.google.common.collect.ImmutableSetMultimap.Builder<Info, Identifier> b2 = ImmutableSetMultimap
-					.builder();
-			for (Identifier id : ds.getAncillary().keySet()) {
-				b2.put((Info) ds.getAncillary().get(id), id);
-			}
-			ImmutableSetMultimap<Info, Identifier> multi = b2.build();
-			for (Info inf : multi.keySet())
-				System.out.println(inf + "=" + multi.get(inf));
+        try {
+            // skip header line
+            br.readLine();
+            String lastFixId = null;
+            Builder<TimedIdentifier> builder = ImmutableSet.builder();
+            long count = 0;
+            long start = System.currentTimeMillis();
+            System.out.println(new Date());
+            Info info = null;
+            while ((line = br.readLine()) != null) {
+                String[] items = line.split("\t");
+                String fixId = items[0];
+                String name = items[1];
+                String value = items[2];
+                info = new Info();
+                info.time = df.parse(items[3]).getTime();
+                info.lat = new BigDecimal(items[4]);
+                info.lon = new BigDecimal(items[5]);
+                if (!name.equals("IMO Number")
+                        || (name.equals("IMO Number") && value.length() == 7)) {
+                    // System.out.println(fixId + "," + name + "," + value + ","
+                    // + new Date(time));
+                    MyIdentifierType type = types.get(name);
+                    Preconditions.checkNotNull(type, name + " not in list");
+                    MyIdentifier id = new MyIdentifier(type, value);
+                    MyTimedIdentifier ti = new MyTimedIdentifier(id, info.time);
+                    count++;
+                    if (lastFixId != null && !fixId.equals(lastFixId)) {
+                        ImmutableSet<TimedIdentifier> set = builder.build();
+                        if (set.size() > 0)
+                            ds = ds.add(set, Preconditions.checkNotNull(info));
+                        builder = ImmutableSet.builder();
+                    }
+                    builder.add(ti);
+                    lastFixId = fixId;
+                    if (count % 5000 == 0) {
+                        System.out.println("sets =" + ds.sets().size());
+                        System.out.println(count);
+                    }
+                }
+            }
+            ds.add(builder.build(), info);
+            System.out.println("sets =" + ds.sets().size());
+            System.out.println(count);
+            long durationMs = System.currentTimeMillis() - start;
+            System.out.println(durationMs);
+            System.out.println(count / 1.0 / durationMs * 1000);
+            com.google.common.collect.ImmutableSetMultimap.Builder<Info, Identifier> b2 = ImmutableSetMultimap
+                    .builder();
+            for (Identifier id : ds.getAncillary().keySet()) {
+                b2.put((Info) ds.getAncillary().get(id), id);
+            }
+            ImmutableSetMultimap<Info, Identifier> multi = b2.build();
+            for (Info inf : multi.keySet())
+                System.out.println(inf + "=" + multi.get(inf));
 
-			PositionsToKmlConverter converter = new PositionsToKmlConverter(
-					"CTS Engine v2");
-			for (Info inf : multi.keySet()) {
-				MyIdentifier id = (MyIdentifier) multi.get(inf).iterator()
-						.next();
-				StringBuffer s = new StringBuffer();
-				for (Identifier ident : multi.get(inf)) {
-					s
-							.append(((MyIdentifierType) ident
-									.getIdentifierType()).getName()
-									+ "="
-									+ ((MyIdentifier) ident).getValue()
-									+ "<br/>");
-				}
-				converter.add(inf.lat, inf.lon, ((MyIdentifierType) id
-						.getIdentifierType()).getName()
-						+ "=" + id.getValue(), s.toString(), null);
-			}
-			String kml = new KmlMarshaller().getKmlAsString(converter
-					.getKmlType(), true);
-			FileOutputStream fos = new FileOutputStream("target/fixes.kml");
-			fos.write(kml.getBytes());
-			fos.close();
+            PositionsToKmlConverter converter = new PositionsToKmlConverter(
+                    "CTS Engine v2");
+            for (Info inf : multi.keySet()) {
+                MyIdentifier id = (MyIdentifier) multi.get(inf).iterator()
+                        .next();
+                StringBuffer s = new StringBuffer();
+                for (Identifier ident : multi.get(inf)) {
+                    s
+                            .append(((MyIdentifierType) ident
+                                    .getIdentifierType()).getName()
+                                    + "="
+                                    + ((MyIdentifier) ident).getValue()
+                                    + "<br/>");
+                }
+                s.append(new Date(inf.time) + "<br/>");
+                converter.add(inf.lat, inf.lon, ((MyIdentifierType) id
+                        .getIdentifierType()).getName()
+                        + "=" + id.getValue(), s.toString(), null);
+            }
+            String kml = new KmlMarshaller().getKmlAsString(converter
+                    .getKmlType(), true);
+            FileOutputStream fos = new FileOutputStream("target/fixes.kml");
+            fos.write(kml.getBytes());
+            fos.close();
 
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		} catch (ParseException e) {
-			throw new RuntimeException(e);
-		}
-	}
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	private static class Info {
-		long time;
-		BigDecimal lat;
-		BigDecimal lon;
+    private static class Info {
+        long time;
+        BigDecimal lat;
+        BigDecimal lon;
 
-		@Override
-		public String toString() {
-			return "Info [lat=" + lat + ", lon=" + lon + ", time="
-					+ new Date(time) + "]";
-		}
-	}
+        @Override
+        public String toString() {
+            return "Info [lat=" + lat + ", lon=" + lon + ", time="
+                    + new Date(time) + "]";
+        }
+    }
 
-	public static void main(String[] args) throws FileNotFoundException {
-		new Cts().load(new FileInputStream("/home/dave/Desktop/fixes.txt"));
-	}
+    public static void main(String[] args) throws FileNotFoundException {
+        new Cts().load(new FileInputStream("target/fixes.txt"));
+    }
 
 }
