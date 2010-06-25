5ecad2329bc229799fbeca43c9067b2710d10329
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-06-25 05:14:13 +0000

    doco enhancements

--- moten-util-parent/trunk/moten-util/src/main/java/moten/david/matchstack/types/impl/MyTimedIdentifier.java
+++ moten-util-parent/trunk/moten-util/src/main/java/moten/david/matchstack/types/impl/MyTimedIdentifier.java
@@ -1,6 +1,7 @@
 package moten.david.matchstack.types.impl;
 
-import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 import moten.david.matchstack.types.TimedIdentifier;
 
@@ -8,71 +9,71 @@ import moten.david.matchstack.types.TimedIdentifier;
  * Implementation of a {@link TimedIdentifier}.
  * 
  * @author dave
- * 
  */
 public class MyTimedIdentifier implements TimedIdentifier {
 
-	private final long time;
-	private final MyIdentifier identifier;
+    private final long time;
+    private final MyIdentifier identifier;
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((identifier == null) ? 0 : identifier.hashCode());
-		result = prime * result + (int) (time ^ (time >>> 32));
-		return result;
-	}
+    private static final DateFormat df = new SimpleDateFormat("MMM dd HH:mm");
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		MyTimedIdentifier other = (MyTimedIdentifier) obj;
-		if (identifier == null) {
-			if (other.identifier != null)
-				return false;
-		} else if (!identifier.equals(other.identifier))
-			return false;
-		if (time != other.time)
-			return false;
-		return true;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((identifier == null) ? 0 : identifier.hashCode());
+        result = prime * result + (int) (time ^ (time >>> 32));
+        return result;
+    }
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param identifier
-	 * @param time
-	 */
-	public MyTimedIdentifier(MyIdentifier identifier, long time) {
-		this.identifier = identifier;
-		this.time = time;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        MyTimedIdentifier other = (MyTimedIdentifier) obj;
+        if (identifier == null) {
+            if (other.identifier != null)
+                return false;
+        } else if (!identifier.equals(other.identifier))
+            return false;
+        if (time != other.time)
+            return false;
+        return true;
+    }
 
-	@Override
-	public MyIdentifier getIdentifier() {
-		return identifier;
-	}
+    /**
+     * Constructor.
+     * 
+     * @param identifier
+     * @param time
+     */
+    public MyTimedIdentifier(MyIdentifier identifier, long time) {
+        this.identifier = identifier;
+        this.time = time;
+    }
 
-	@Override
-	public long getTime() {
-		return time;
-	}
+    @Override
+    public MyIdentifier getIdentifier() {
+        return identifier;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return identifier.toString() + "(" + (new Date(time)) + ")";
-	}
+    @Override
+    public long getTime() {
+        return time;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return identifier.toString() + "(" + time + ")";
+    }
 
 }
