89afa1e5e6d5b08a85e9a50ac0b89b60d160dc1c
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-07-06 13:01:55 +0000

    ets, 400 response for bad request

--- ets/trunk/src/main/java/moten/david/ets/server/FixServlet.java
+++ ets/trunk/src/main/java/moten/david/ets/server/FixServlet.java
@@ -36,47 +36,55 @@ public class FixServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
-        String ids = Preconditions
-                .checkNotNull(
-                        request.getParameter("ids"),
-                        "ids parameter cannot be null and should contain name value pairs with colon ':' delimiting name and value and the pairs delimited by semicolon ';'");
-        System.out.println("ids=" + ids);
-        String[] items = ids.split(";");
-        Builder<String, String> builder = ImmutableMap.builder();
-        for (String item : items) {
-            String[] parts = item.split(":");
-            String name = parts[0];
-            String value = parts[1];
-            builder.put(name, value);
-        }
-        Long time = Long
-                .parseLong(Preconditions
-                        .checkNotNull(request.getParameter("time"),
-                                "time parameter must be specified (long UNIX time value in ms)"));
-
         try {
-            double lat = Double.parseDouble(Preconditions.checkNotNull(request
-                    .getParameter("lat"), "parameter cannot be null: lat"));
+            String ids = Preconditions
+                    .checkNotNull(
+                            request.getParameter("ids"),
+                            "ids parameter cannot be null and should contain name value pairs with colon ':' delimiting name and value and the pairs delimited by semicolon ';'");
+            System.out.println("ids=" + ids);
+            String[] items = ids.split(";");
+            Builder<String, String> builder = ImmutableMap.builder();
+            for (String item : items) {
+                String[] parts = item.split(":");
+                String name = parts[0];
+                String value = parts[1];
+                builder.put(name, value);
+            }
+            Long time = Long
+                    .parseLong(Preconditions
+                            .checkNotNull(request.getParameter("time"),
+                                    "time parameter must be specified (long UNIX time value in ms)"));
 
-            double lon = Double.parseDouble(Preconditions.checkNotNull(request
-                    .getParameter("lon"), "parameter cannot be null: lon"));
-            Fix fix = new Fix();
-            fix.setId(System.currentTimeMillis());
-            fix.setLat(lat);
-            fix.setLon(lon);
-            fix.setTime(new Date(time));
-            MyFix f = new MyFix(fix, builder.build());
-            entities.add(ImmutableList.of(f));
-        } catch (NumberFormatException ex) {
-            throw new RuntimeException("a parameter is not a valid number", ex);
+            try {
+                double lat = Double.parseDouble(Preconditions.checkNotNull(
+                        request.getParameter("lat"),
+                        "parameter cannot be null: lat"));
+
+                double lon = Double.parseDouble(Preconditions.checkNotNull(
+                        request.getParameter("lon"),
+                        "parameter cannot be null: lon"));
+                Fix fix = new Fix();
+                fix.setId(System.currentTimeMillis());
+                fix.setLat(lat);
+                fix.setLon(lon);
+                fix.setTime(new Date(time));
+                MyFix f = new MyFix(fix, builder.build());
+                entities.add(ImmutableList.of(f));
+            } catch (NumberFormatException ex) {
+                throw new RuntimeException("a parameter is not a valid number",
+                        ex);
+            }
+            QueryResultIterator<MyEntity> e = datastore.find(MyEntity.class);
+            int count = 0;
+            while (e.hasNext()) {
+                e.next();
+                count++;
+            }
+            response.getOutputStream().println(
+                    "there are " + count + " entities eh");
+        } catch (RuntimeException e) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.getOutputStream().println(e.getMessage());
         }
-        QueryResultIterator<MyEntity> e = datastore.find(MyEntity.class);
-        int count = 0;
-        while (e.hasNext()) {
-            e.next();
-            count++;
-        }
-        response.getOutputStream().println(
-                "there are " + count + " entities eh");
     }
 }
