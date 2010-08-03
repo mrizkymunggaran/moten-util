ee3703bf5c614d7ca55e5076212dd902213a8726
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-08-03 12:41:45 +0000

    using a set of URL is a performance no no because equals and hashCode both try to resolve domains for a URL

--- moten-util-parent/trunk/moten-util/src/main/java/moten/david/util/monitoring/lookup/CachingUrlPropertiesProvider.java
+++ moten-util-parent/trunk/moten-util/src/main/java/moten/david/util/monitoring/lookup/CachingUrlPropertiesProvider.java
@@ -1,6 +1,8 @@
 package moten.david.util.monitoring.lookup;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Map;
 import java.util.Properties;
@@ -10,30 +12,34 @@ import com.google.inject.Provider;
 
 public class CachingUrlPropertiesProvider {
 
-	private final Map<URL, Properties> map = new ConcurrentHashMap<URL, Properties>();
+    private final Map<URI, Properties> map = new ConcurrentHashMap<URI, Properties>();
 
-	public Provider<Properties> getPropertiesProvider(
-			final UrlFactory urlFactory, final String path) {
-		return new Provider<Properties>() {
+    public Provider<Properties> getPropertiesProvider(
+            final UrlFactory urlFactory, final String path) {
+        return new Provider<Properties>() {
 
-			@Override
-			public Properties get() {
-				URL url = urlFactory.create(path);
-				return getProperties(url);
-			}
-		};
-	}
+            @Override
+            public Properties get() {
+                URL url = urlFactory.create(path);
+                return getProperties(url);
+            }
+        };
+    }
 
-	protected synchronized Properties getProperties(URL url) {
-		if (map.get(url) == null) {
-			Properties p = new Properties();
-			try {
-				p.load(url.openStream());
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
-			map.put(url, p);
-		}
-		return map.get(url);
-	}
+    protected synchronized Properties getProperties(URL url) {
+        try {
+            if (map.get(url.toURI()) == null) {
+                Properties p = new Properties();
+                try {
+                    p.load(url.openStream());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                map.put(url.toURI(), p);
+            }
+            return map.get(url.toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
