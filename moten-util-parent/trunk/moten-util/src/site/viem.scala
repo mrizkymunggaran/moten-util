49a846c15628b12574a7a28c4260c4e85f1bb648
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-11-08 19:14:56 +0000

    viem dev

--- moten-util-parent/trunk/moten-util/src/site/viem.scala
+++ moten-util-parent/trunk/moten-util/src/site/viem.scala
@@ -1,4 +1,5 @@
 import java.util.Date
+import scala.collection.immutable._
 
 case class IdentifierType(name:String) extends Ordered[IdentifierType] {
 	def compare(that:IdentifierType):Int = 
@@ -21,9 +22,36 @@ case class TimedIdentifier(id:Identifier, time:Date) extends Ordered[TimedIdenti
 			this.id.compare(that.id)
 }
 
-class System(q:Set[TimedIdentifier], e:TimedIdentifier => TimedIdentifier) {
-	def add(a:SortedSet[TimedIdentifier]):System {
-		new System(q,e)
+trait Domain[A] {
+	def domain: Set[A]
+}
+
+trait EntityFunction extends Function[TimedIdentifier,Set[TimedIdentifier]] with Domain[TimedIdentifier]
+
+class MyEntityFunction(map:Map[TimedIdentifier,Set[TimedIdentifier]]) extends EntityFunction {
+	def apply(i:TimedIdentifier) = map.get(i) match {
+		case si:Some[Set[TimedIdentifier]]  => si.get
+		case _ => throw new NullPointerException 
+	}
+	def domain : Set[TimedIdentifier]= map.keySet 
+	
+}
+
+class System(e:EntityFunction) {
+
+	def add(a:SortedSet[TimedIdentifier]):System = {
+		val a1 = a.max //assuming strongest is max
+		val b = beta(e.domain,a1)				
+		val c = a.map(gamma(e(a1),_)).flatten
+		val domain2 = e.domain.filter(!b.contains(_)).filter(!c.contains(_))
+		
+		new System(e)
+	}	
+	private def beta(set:Set[TimedIdentifier],i:TimedIdentifier) = {
+		set.filter( _.id == i.id).filter( _.time.compareTo(i.time)<0)
+	}
+	private def gamma(set:Set[TimedIdentifier],i:TimedIdentifier) = {
+		set.filter( _.id.typ == i.id.typ).filter( _.time.compareTo(i.time)<0)
 	}
 }
 
