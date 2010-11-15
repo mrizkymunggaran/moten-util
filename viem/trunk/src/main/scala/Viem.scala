ff16cc81de4dad8bc3f8f610c6351fbc73afe0d2
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-11-15 06:44:54 +0000

    viem dev

--- viem/trunk/src/main/scala/Viem.scala
+++ viem/trunk/src/main/scala/Viem.scala
@@ -137,42 +137,5 @@ class System {
 		}
     }   
 
-    object System {
-
-        def empty = MetaSet(Set(),MetaData("empty"))
-
-        def create(name:String, value:String, time:Date) =
-             TimedIdentifier(Identifier(IdentifierType(name),value),time)
-
-        def assertEquals(x:AnyRef, y:AnyRef) {
-            if (x!=y)
-                throw new RuntimeException("assert equals failed")
-        }
-
-        def test() {
-            val date = new Date()
-            val date2 = new Date(0)
-            val m = "m"
-            val g = "g"
-            val a1 = create("m","1", date)
-            val a2 = create("g","2", date)
-            val b1 = create("m","1", date2)
-            val b2 = create("g","3", date2)
-            println(a1)
-            println(a2)
-            println(Set(a2,a1).max)
-            
-            //check that max works
-            assertEquals(
-                    TimedIdentifier(Identifier(IdentifierType(m),"1"),date)                 ,
-                    Set(a2,a1).max)
-            assertEquals(
-                    TimedIdentifier(Identifier(IdentifierType(m),"1"),date)                 ,
-                    Set(a1,a2).max)                             
-
-            println(new System().merge(a1,a2,MetaData("a"), MetaSet(Set(b1,b2),MetaData("b")), empty))
-            println("******************\nfinished tests")
-        }
-    }
 }
 
--- viem/trunk/src/test/scala/AppTest.scala
+++ viem/trunk/src/test/scala/AppTest.scala
@@ -6,12 +6,38 @@ import Assert._
 @Test
 class AppTest {
 
+	def empty = MetaSet(Set(),MetaData("empty"))
+    def create(name:String, value:String, time:Date) =
+        TimedIdentifier(Identifier(IdentifierType(name),value),time)
+
     @Test
     def testOK() = assertTrue(true)
 
-//    @Test
-//    def testKO() = assertTrue(false)
+    @Test
+    def testSystem() = {
+		val date = new Date()
+		val date2 = new Date(0)
+		val m = "m"
+		val g = "g"
+		val a1 = create("m","1", date)
+		val a2 = create("g","2", date)
+		val b1 = create("m","1", date2)
+		val b2 = create("g","3", date2)
+		println(a1)
+		println(a2)
+		println(Set(a2,a1).max)
 
+		//check that max works
+		assertEquals(
+				TimedIdentifier(Identifier(IdentifierType(m),"1"),date)                 ,
+				Set(a2,a1).max)
+		assertEquals(
+				TimedIdentifier(Identifier(IdentifierType(m),"1"),date)                 ,
+				Set(a1,a2).max)                             
+
+		println(new System().merge(a1,a2,MetaData("a"), MetaSet(Set(b1,b2),MetaData("b")), empty))
+		println("******************\nfinished tests")
+	}
 }
 
 
