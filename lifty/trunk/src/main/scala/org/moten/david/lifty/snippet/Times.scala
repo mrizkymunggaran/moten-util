ae19fad4ac11ff883d3120435f822461c1b0f2a8
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-12-04 01:59:11 +0000

    lifty dev

--- lifty/trunk/src/main/scala/org/moten/david/lifty/snippet/Times.scala
+++ lifty/trunk/src/main/scala/org/moten/david/lifty/snippet/Times.scala
@@ -12,23 +12,45 @@ package snippet {
   import SHtml._
   import _root_.java.util._
 
+  case class TimeEntry(date: Date, start: String, finish: String)
+
   class Times {
-    val df = new java.text.SimpleDateFormat("EEE d MMM yyyy")
+    //make df a def not a val because not thread safe
+    def df = new java.text.SimpleDateFormat("EEE d MMM yyyy")
     object date extends RequestVar(new Date())
     object start extends RequestVar("08301230")
     object finish extends RequestVar("")
     object item extends SessionVar("")
-    def nextDate(s: String): String =
-      df.format(new Date(df.parse(s).getTime() + 24 * 60 * 60 * 1000L))
+    
+    def nextDay(d:Date): Date =
+      new Date(d.getTime() + 24 * 60 * 60 * 1000L)
 
     def add(xhtml: NodeSeq): NodeSeq = {
       def processEntryAdd() {
+          error("boo")
       }
       bind("entry", xhtml,
         "date" -%> text(df.format(date.get),
-          x => date(df.parse(x))),
-        "start" -%> ajaxText(start, { value => SetHtml("list", "gotcha") }),
-        "finish" -%> text(finish, finish(_)))
+          x => date(nextDay(df.parse(x)))),
+        "start" -%> text(start,start(_)),
+        "finish" -%> text(finish, finish(_)),
+        "submit" -%> submit("submit",processEntryAdd))
+    }
+
+    def list(in: NodeSeq): NodeSeq = {
+      val df = new java.text.SimpleDateFormat("EEE d MMM yyyy");
+      val toRender = (1 to 10).map(
+        i => TimeEntry(new Date(), "0839", "1230"))
+      toRender.flatMap { item =>
+        def doRow(template: NodeSeq): NodeSeq = {
+          bind(
+            "entry", template,
+            "date" -> Text(df.format(item.date)),
+            "start" -> Text(item.start),
+            "finish" -> Text(item.finish))
+        }
+        bind("entry", in, "row" -> doRow _)
+      }
     }
 
   }
