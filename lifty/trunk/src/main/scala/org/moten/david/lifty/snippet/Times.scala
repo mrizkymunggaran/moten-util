f3cbb9eae7c916e3332686ee50bf73b91439b425
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-12-04 01:19:38 +0000

    lifty dev

--- lifty/trunk/src/main/scala/org/moten/david/lifty/snippet/Times.scala
+++ lifty/trunk/src/main/scala/org/moten/david/lifty/snippet/Times.scala
@@ -10,21 +10,27 @@ package snippet {
   import JsCmds._
   import JE._
   import SHtml._
+  import _root_.java.util._
 
   class Times {
-    object date extends RequestVar("1 Jan 2010")
-    object start extends RequestVar("0830")
-    object finish extends RequestVar("1230")
+    val df = new java.text.SimpleDateFormat("EEE d MMM yyyy")
+    object date extends RequestVar(new Date())
+    object start extends RequestVar("08301230")
+    object finish extends RequestVar("")
     object item extends SessionVar("")
+    def nextDate(s: String): String =
+      df.format(new Date(df.parse(s).getTime() + 24 * 60 * 60 * 1000L))
+
     def add(xhtml: NodeSeq): NodeSeq = {
       def processEntryAdd() {
       }
       bind("entry", xhtml,
-        "date" -%> text(date, date(_)),
-        "start" -%> ajaxText(start, {value => SetHtml("list","gotcha")}),
+        "date" -%> text(df.format(date.get),
+          x => date(df.parse(x))),
+        "start" -%> ajaxText(start, { value => SetHtml("list", "gotcha") }),
         "finish" -%> text(finish, finish(_)))
     }
-  
+
   }
   object Times {
     var number: Double = 0
