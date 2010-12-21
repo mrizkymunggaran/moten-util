50a6cb00a6dc014afda103d34c564eef71fc1c82
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2010-12-21 11:30:38 +0000

    geo-svg dev

---
+++ geo-svg/trunk/src/main/java/ScreenPoint.java
@@ -0,0 +1,19 @@
+public class ScreenPoint {
+	private final int x;
+	private final int y;
+
+	public ScreenPoint(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+}
