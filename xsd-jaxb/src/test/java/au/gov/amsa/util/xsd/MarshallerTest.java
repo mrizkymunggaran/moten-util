459da0f51467ca99878f9b8126c58679af28bb8e
davidmoten@gmail.com <davidmoten@gmail.com@5f29f07a-d3f3-11de-9f39-755c91dee682>
2011-03-02 03:27:56 +0000

    initial commit of xsd-jaxb

---
+++ xsd-jaxb/src/test/java/au/gov/amsa/util/xsd/MarshallerTest.java
@@ -0,0 +1,29 @@
+package au.gov.amsa.util.xsd;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.w3._2001.xmlschema.ObjectFactory;
+import org.w3._2001.xmlschema.Schema;
+public class MarshallerTest {
+
+	@Test
+	public void testUnmarshal() {
+		try {
+			JAXBContext context = JAXBContext.newInstance(ObjectFactory.class);
+			Unmarshaller u = context.createUnmarshaller();
+			StreamSource source = new StreamSource(MarshallerTest.class.getResourceAsStream("/test.xsd"));
+			JAXBElement<Schema> element = u.unmarshal(source, Schema.class);
+			Schema schema = element.getValue();
+			Assert.assertEquals("http://www.amsa.gov.au/craft-position/",  schema.getTargetNamespace());
+		} catch (JAXBException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+}
