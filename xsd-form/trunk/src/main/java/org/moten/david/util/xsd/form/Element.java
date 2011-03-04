d69fd206bae2cdf0bc0b16d5bde792842baa74fe
davidmoten <davidmoten@gmail.com>
2011-03-04 00:37:49 +0000

    xsd-form dev

--- xsd-form/trunk/src/main/java/org/moten/david/util/xsd/form/Element.java
+++
@@ -1,9 +0,0 @@
-package org.moten.david.util.xsd.form;
-
-public class Element implements Part {
-	private XsdString name;
-	private final int minOccurs = 1;
-	private final int maxOccurs = 1;
-	private final boolean maxUnbounded = false;
-	private Type type;
-}
