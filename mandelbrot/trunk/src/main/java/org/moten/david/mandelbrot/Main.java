4eaa9c4a9cd706de7cd442c03c9190e55ff4ed44
davidmoten <davidmoten@gmail.com>
2011-05-03 05:17:16 +0000

    mandelbrot dev

--- mandelbrot/trunk/src/main/java/org/moten/david/mandelbrot/Main.java
+++ mandelbrot/trunk/src/main/java/org/moten/david/mandelbrot/Main.java
@@ -1,6 +1,9 @@
 package org.moten.david.mandelbrot;
 
+import java.awt.Color;
 import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 
@@ -10,7 +13,25 @@ public class Main {
 		frame.setSize(740, 740);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new GridLayout(1, 1));
-		MandelbrotFractal fractal = new MandelbrotFractal(256, 1);
+		final Color brown = new Color(205, 183, 158);
+		final Color salmon = new Color(250, 128, 114);
+		List<Color> browny = new ArrayList<Color>() {
+			{
+				add(brown);
+				add(Color.white);
+				add(salmon);
+				add(Color.black);
+			}
+		};
+		List<Color> wikipedia = new ArrayList<Color>() {
+			{
+				add(Color.blue);
+				add(Color.white);
+				add(Color.yellow);
+				add(Color.black);
+			}
+		};
+		MandelbrotFractal fractal = new MandelbrotFractal(512, 1, wikipedia);
 		frame.getContentPane().add(fractal);
 		// frame.pack();
 		frame.setVisible(true);
