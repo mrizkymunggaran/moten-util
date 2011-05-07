424f6998a42bc36277f948821fedb55559d1e372
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2011-05-07 21:21:19 +0000

    refactored mandelbrot

--- mandelbrot/trunk/src/main/java/org/moten/david/mandelbrot/ViewerMain.java
+++ mandelbrot/trunk/src/main/java/org/moten/david/mandelbrot/ViewerMain.java
@@ -12,7 +12,7 @@ import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
-public class Main {
+public class ViewerMain {
 	public static void main(String[] args) {
 		final JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
