05ae917aa8ea02938684658a0eb01d8c04af1d41
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2011-05-09 04:28:45 +0000

    Share project "physics" into "https://moten-util.googlecode.com/svn"

---
+++ physics/trunk/src/main/java/org/moten/david/physics/fluids/EkmanTransport.java
@@ -0,0 +1,85 @@
+package org.moten.david.physics.fluids;
+
+public class EkmanTransport {
+
+	// All units are the SI units for the measure unless otherwise stated
+
+	private static final double DENSITY_OF_AIR = 1.3;
+
+	private static final double ROTATION_RATE_OF_EARTH_RADS_PER_SEC = 0.000072921;
+
+	/**
+	 * <p>
+	 * Returns wind stress calculated according to <a href=
+	 * "http://oceanworld.tamu.edu/resources/ocng_textbook/chapter04/chapter04_05.htm"
+	 * >here</a>
+	 * </p>
+	 * 
+	 * @param windSpeedAt10Metres
+	 * @return
+	 */
+	private double getWindStress(double windSpeedAt10Metres) {
+		return DENSITY_OF_AIR * getDragCoefficient(windSpeedAt10Metres)
+				* sq(windSpeedAt10Metres);
+	}
+
+	/**
+	 * <p>
+	 * Returns drag coefficient calculated according to <a href=
+	 * "http://oceanworld.tamu.edu/resources/ocng_textbook/chapter04/chapter04_05.htm"
+	 * >here</a> using the Smith 1980 formula.
+	 * </p>
+	 * 
+	 * @param windSpeedAt10Metres
+	 * @return
+	 */
+	private double getDragCoefficient(double windSpeedAt10Metres) {
+		// Smith 1980 formula
+		return 0.44 + 0.063 * windSpeedAt10Metres;
+	}
+
+	private double getEddyViscosity() {
+		return 1.0e-05;
+	}
+
+	private void calculateEkmanTransport(double ekmanDepth,
+			double coriolisParameter, double windSpeedAt10Metres,
+			boolean isNorthernHemisphere, double waterDensity, double z,
+			double[] result) {
+		double a = Math.PI / ekmanDepth * z;
+		double b = a + Math.PI / 4;
+		double v0 = Math.sqrt(2 * Math.PI * getWindStress(windSpeedAt10Metres))
+				/ ekmanDepth / coriolisParameter / waterDensity;
+		int sign = (isNorthernHemisphere ? 1 : -1);
+		double u = sign * v0 * Math.cos(b) * Math.exp(a);
+
+		double v = v0 * Math.sin(b) * Math.exp(a);
+		result[0] = u;
+		result[1] = v;
+	}
+
+	private void calculateEkmanTransport(double latitudeDegrees,
+			double windSpeedAt10Metres, double waterDensity, double z,
+			double[] result) {
+		double ekmanDepth = getEkmanDepth(latitudeDegrees);
+		double f = getCoriolisParameter(latitudeDegrees);
+		calculateEkmanTransport(ekmanDepth, f, windSpeedAt10Metres,
+				latitudeDegrees > 0, waterDensity, z, result);
+	}
+
+	private double getCoriolisParameter(double latitudeDegrees) {
+		return 2 * ROTATION_RATE_OF_EARTH_RADS_PER_SEC
+				* Math.sin(Math.toRadians(latitudeDegrees));
+	}
+
+	private double getEkmanDepth(double latitudeDegrees) {
+		return Math.PI
+				* Math.sqrt(2 * getEddyViscosity()
+						/ Math.abs(getCoriolisParameter(latitudeDegrees)));
+	}
+
+	private static double sq(double d) {
+		return d * d;
+	}
+
+}
