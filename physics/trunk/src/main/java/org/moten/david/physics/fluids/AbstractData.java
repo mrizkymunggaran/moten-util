5c01c29d5e03803d8607d35109d72fb2780db76c
davidmoten@gmail.com <davidmoten@gmail.com@5f29f07a-d3f3-11de-9f39-755c91dee682>
2011-05-13 07:54:02 +0000

    physics dev

---
+++ physics/trunk/src/main/java/org/moten/david/physics/fluids/AbstractData.java
@@ -0,0 +1,39 @@
+package org.moten.david.physics.fluids;
+
+import org.moten.david.physics.fluids.Vector.Direction;
+import org.moten.david.util.math.Function;
+
+public abstract class AbstractData implements Data {
+
+	private final Function<Vector, Vector> velocityField = new Function<Vector, Vector>() {
+		Function<Vector, Double> u = getField(Direction.X);
+		Function<Vector, Double> v = getField(Direction.Y);
+		Function<Vector, Double> w = getField(Direction.Z);
+
+		@Override
+		public Vector apply(Vector point) {
+			return new Vector(u.apply(point), v.apply(point), w.apply(point));
+		}
+	};
+
+	@Override
+	public abstract Function<Vector, Double> getDensity();
+
+	@Override
+	public abstract Function<Vector, Double> getField(Direction direction);
+
+	@Override
+	public final Function<Vector, Vector> getVelocityField() {
+		return velocityField;
+	}
+
+	@Override
+	public abstract Function<Vector, Double> getPressure();
+
+	@Override
+	public abstract WallFinder getWallFinder(Direction direction);
+
+	@Override
+	public abstract Function<Vector, Double> getDynamicViscosity();
+
+}
