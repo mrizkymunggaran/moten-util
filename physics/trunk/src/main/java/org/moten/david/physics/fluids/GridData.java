60685178e3afb2e9822179b3cbeefaae8b2cf210
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2011-05-14 23:55:33 +0000

    physics dev

--- physics/trunk/src/main/java/org/moten/david/physics/fluids/GridData.java
+++ physics/trunk/src/main/java/org/moten/david/physics/fluids/GridData.java
@@ -9,67 +9,121 @@ import org.moten.david.util.math.Vector;
 
 public class GridData implements Data {
 
+	private Grid<Value> grid;
+
 	@Override
 	public Value getValue(Vector position) {
-		// TODO Auto-generated method stub
-		return null;
+		return grid.get(position);
 	}
 
 	private Vector getNeighbour(Vector position, Direction direction,
 			boolean positive) {
-		// TODO
-		return null;
+		return grid.getNeighbour(position, direction, positive);
+	}
+
+	private static class Neighbours {
+		Vector x1, x2, y1, y2, z1, z2;
+		public Value valueX1, valueX2, valueY1, valueY2, valueZ1, valueZ2;
+	}
+
+	private Neighbours getNeighbours(Vector position, Value wallValue) {
+		Neighbours n = new Neighbours();
+		n.x1 = getNeighbour(position, Direction.X, false);
+		n.x2 = getNeighbour(position, Direction.X, true);
+		n.y1 = getNeighbour(position, Direction.Y, false);
+		n.y2 = getNeighbour(position, Direction.Y, true);
+		n.z1 = getNeighbour(position, Direction.Z, false);
+		n.z2 = getNeighbour(position, Direction.Z, true);
+		n.valueX1 = getValue(n.x1, wallValue);
+		n.valueX2 = getValue(n.x2, wallValue);
+		n.valueY1 = getValue(n.y1, wallValue);
+		n.valueY2 = getValue(n.y2, wallValue);
+		n.valueZ1 = getValue(n.z1, wallValue);
+		n.valueZ2 = getValue(n.z2, wallValue);
+		return n;
 	}
 
 	@Override
 	public Vector getPressureGradient(Vector position) {
-		double pressure = getValue(position).pressure;
-		Vector x1 = getNeighbour(position, Direction.X, false);
-		Vector x2 = getNeighbour(position, Direction.X, true);
-		Vector y1 = getNeighbour(position, Direction.Y, false);
-		Vector y2 = getNeighbour(position, Direction.Y, true);
-		Vector z1 = getNeighbour(position, Direction.Z, false);
-		Vector z2 = getNeighbour(position, Direction.Z, true);
-		double pX1 = getPressure(x1, pressure);
-		double pX2 = getPressure(x2, pressure);
-		double pY1 = getPressure(y1, pressure);
-		double pY2 = getPressure(y2, pressure);
-		double pZ1 = getPressure(z1, pressure);
-		double pZ2 = getPressure(z2, pressure);
-
-		double gradX = (pX2 - pX1) / (x2.x - x1.x);
-		double gradY = (pY2 - pY1) / (y2.x - y1.x);
-		double gradZ = (pZ2 - pZ1) / (z2.x - z1.x);
+		Value value = getValue(position);
+		Neighbours n = getNeighbours(position, value);
+		double gradX = (n.valueX2.pressure - n.valueX1.pressure)
+				/ (n.x2.x - n.x1.x);
+		double gradY = (n.valueY2.pressure - n.valueY1.pressure)
+				/ (n.x2.y - n.x1.y);
+		double gradZ = (n.valueZ2.pressure - n.valueZ1.pressure)
+				/ (n.z2.x - n.z1.x);
 		return vector(gradX, gradY, gradZ);
 	}
 
 	/**
-	 * Returns <code>defaultPressure</code> if there is a wall at
-	 * <code>position</code>. Otherwise returns the pressure value at
+	 * Returns <code>wallValue</code> if there is a wall at
+	 * <code>position</code>. Otherwise returns {@link Value} at
 	 * <code>position</code>.
 	 * 
 	 * @param position
 	 * @param defaultPressure
 	 * @return
 	 */
-	private double getPressure(Vector position, double defaultPressure) {
+	private Value getValue(Vector position, Value wallValue) {
 		Value value = getValue(position);
 		if (value.isWall())
-			return defaultPressure;
+			return wallValue;
 		else
-			return value.pressure;
+			return value;
+	}
+
+	private Vector getVelocityDerivative(Vector position, Direction direction) {
+		Value value = getValue(position);
+		Value wallValue = new Value(new Vector(0, 0, 0), value.pressure,
+				value.depth, value.density, value.viscosity);
+		Neighbours n = getNeighbours(position, wallValue);
+		double gradX = (n.valueX2.velocity.get(direction) - n.valueX1.velocity
+				.get(direction)) / (n.x2.x - n.x1.x);
+		double gradY = (n.valueY2.velocity.get(direction) - n.valueY1.velocity
+				.get(direction)) / (n.x2.y - n.x1.y);
+		double gradZ = (n.valueZ2.velocity.get(direction) - n.valueZ1.velocity
+				.get(direction)) / (n.z2.x - n.z1.x);
+		return vector(gradX, gradY, gradZ);
 	}
 
 	@Override
 	public Matrix getVelocityJacobian(Vector position) {
-		// TODO Auto-generated method stub
-		return null;
+		Vector dvdx = getVelocityDerivative(position, Direction.X);
+		Vector dvdy = getVelocityDerivative(position, Direction.Y);
+		Vector dvdz = getVelocityDerivative(position, Direction.Z);
+		Matrix m = new Matrix(dvdx, dvdy, dvdz);
+		return m;
+	}
+
+	private Vector getVelocity2ndDerivative(Vector position, Direction direction) {
+		Value value = getValue(position);
+		Value wallValue = new Value(new Vector(0, 0, 0), value.pressure,
+				value.depth, value.density, value.viscosity);
+		Neighbours n = getNeighbours(position, wallValue);
+		double gradX = (n.valueX2.velocity.get(direction)
+				+ n.valueX1.velocity.get(direction) - 2 * value.velocity
+				.get(direction)) / sqr(n.x2.x - n.x1.x);
+		double gradY = (n.valueY2.velocity.get(direction)
+				+ n.valueY1.velocity.get(direction) - 2 * value.velocity
+				.get(direction)) / sqr(n.y2.x - n.y1.x);
+		double gradZ = (n.valueZ2.velocity.get(direction)
+				+ n.valueZ1.velocity.get(direction) - 2 * value.velocity
+				.get(direction)) / sqr(n.z2.x - n.z1.x);
+		return vector(gradX, gradY, gradZ);
+	}
+
+	private static double sqr(double d) {
+		return d * d;
 	}
 
 	@Override
 	public Vector getVelocityLaplacian(Vector position) {
-		// TODO Auto-generated method stub
-		return null;
+		Vector dvdx = getVelocity2ndDerivative(position, Direction.X);
+		Vector dvdy = getVelocity2ndDerivative(position, Direction.Y);
+		Vector dvdz = getVelocity2ndDerivative(position, Direction.Z);
+		Matrix m = new Matrix(dvdx, dvdy, dvdz);
+		return m.sumColumnVectors();
 	}
 
 	@Override
