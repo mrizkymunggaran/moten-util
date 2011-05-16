fa3dd28b43757d430ed20271c8b6d2e05372fcb6
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2011-05-16 12:29:51 +0000

    physics dev

--- physics/trunk/src/test/java/org/moten/david/physics/fluids/NavierStokesSolverTest.java
+++ physics/trunk/src/test/java/org/moten/david/physics/fluids/NavierStokesSolverTest.java
@@ -12,9 +12,9 @@ public class NavierStokesSolverTest {
 	@Test
 	public void test1() {
 		NavierStokesSolver s = new NavierStokesSolver();
-		Data data = new GridData(new Grid1(val(10, 10, 0, 1000), val(0, 0, 0,
-				1000), val(0, 0, 0, 1000), val(0, 0, 0, 1000), val(0, 0, 0,
-				1000), val(0, 0, 0, 1000), val(0, 0, 0, 1000)));
+		Data data = new GridData(new Grid1(val(10, 10, 0, 1200),
+				val(0, 0, 0, 0), val(0, 0, 0, 0), val(0, 0, 0, 0), val(0, 0, 0,
+						0), val(0, 0, 0, 0), val(0, 0, 0, 0)));
 		System.out.println("laplacian=" + data.getVelocityLaplacian(origin));
 		Value val = s.getValueAfterTime(data, origin, 30 * 60);
 		System.out.println(val);
