c60c835450a45685f4df17a515df1b16afae4ef3
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2011-05-16 12:54:15 +0000

    physics dev

--- physics/trunk/src/test/java/org/moten/david/physics/fluids/NavierStokesSolverTest.java
+++ physics/trunk/src/test/java/org/moten/david/physics/fluids/NavierStokesSolverTest.java
@@ -19,11 +19,18 @@ public class NavierStokesSolverTest {
 						0), val(0, 0, 0, 0), val(0, 0, 0, 0)));
 		System.out.println("laplacian=" + data.getVelocityLaplacian(origin));
 		Value val = s.getValueAfterTime(data, origin, 60);
+		long t = System.currentTimeMillis();
+		long N = 100000;
+		for (int i = 0; i < N; i++) {
+			s.getValueAfterTime(data, origin, 60);
+		}
+		t = System.currentTimeMillis() - t;
 		System.out.println(val);
 		assertEquals(1200, val.pressure, PRECISION);
 		assertEquals(10, val.velocity.x, PRECISION);
 		assertEquals(10, val.velocity.y, PRECISION);
 		assertEquals(-60 * 9.8, val.velocity.z, PRECISION);
+		System.out.println(N + " calcs in " + t / 1000.0 + "s");
 	}
 
 }
