82aadbd068348be33f6b3cac7daeb7e756611c8b
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2011-05-16 12:40:17 +0000

    physics dev

--- physics/trunk/src/test/java/org/moten/david/physics/fluids/NavierStokesSolverTest.java
+++ physics/trunk/src/test/java/org/moten/david/physics/fluids/NavierStokesSolverTest.java
@@ -1,5 +1,6 @@
 package org.moten.david.physics.fluids;
 
+import static org.junit.Assert.assertEquals;
 import static org.moten.david.physics.fluids.Util.val;
 
 import org.junit.Test;
@@ -7,6 +8,7 @@ import org.moten.david.util.math.Vector;
 
 public class NavierStokesSolverTest {
 
+	private static final double PRECISION = 0.01;
 	private static Vector origin = new Vector(0, 0, 0);
 
 	@Test
@@ -16,8 +18,12 @@ public class NavierStokesSolverTest {
 				val(0, 0, 0, 0), val(0, 0, 0, 0), val(0, 0, 0, 0), val(0, 0, 0,
 						0), val(0, 0, 0, 0), val(0, 0, 0, 0)));
 		System.out.println("laplacian=" + data.getVelocityLaplacian(origin));
-		Value val = s.getValueAfterTime(data, origin, 30 * 60);
+		Value val = s.getValueAfterTime(data, origin, 60);
 		System.out.println(val);
+		assertEquals(1200, val.pressure, PRECISION);
+		assertEquals(10, val.velocity.x, PRECISION);
+		assertEquals(10, val.velocity.y, PRECISION);
+		assertEquals(-60 * 9.8, val.velocity.z, PRECISION);
 	}
 
 }
