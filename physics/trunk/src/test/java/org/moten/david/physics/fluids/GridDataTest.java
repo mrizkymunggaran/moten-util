a2e4c30ff0b1d77f3a80f94c6d51ca9e82dcd8b8
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2011-05-16 11:54:06 +0000

    physics dev

--- physics/trunk/src/test/java/org/moten/david/physics/fluids/GridDataTest.java
+++ physics/trunk/src/test/java/org/moten/david/physics/fluids/GridDataTest.java
@@ -1,6 +1,10 @@
 package org.moten.david.physics.fluids;
 
+import static org.junit.Assert.assertEquals;
 import static org.moten.david.physics.fluids.Testing.val;
+import static org.moten.david.util.math.Direction.X;
+import static org.moten.david.util.math.Direction.Y;
+import static org.moten.david.util.math.Direction.Z;
 import static org.moten.david.util.math.Vector.vector;
 
 import org.junit.Assert;
@@ -34,17 +38,50 @@ public class GridDataTest {
 				20, 20), val(100, 110, 120), val(200, 200, 200), val(1000,
 				1100, 1200), val(2000, 2000, 2000)));
 		Matrix m = data.getVelocityJacobian(origin);
+		Matrix expected = new Matrix(vector(5.0, 50.0, 500.0), vector(4.5, 45,
+				450), vector(4, 40, 400));
 		System.out.println(m);
+		Assert.assertEquals(expected, m);
+	}
+
+	@Test
+	public void testVelocity2ndDerivative() {
+
+		GridData data = new GridData(new Grid1(val(100, 100, 100, 1000), val(
+				10, 11, 12), val(20, 20, 20), val(100, 110, 120), val(200, 200,
+				200), val(1000, 1100, 1200), val(2000, 2000, 2000)));
+		{
+			Vector v = data.getVelocity2ndDerivative(origin, X);
+			System.out.println("2nd derivative dx=" + v);
+			assertEquals(-42.5, v.x, PRECISION);
+			assertEquals(25, v.y, PRECISION);
+			assertEquals(700, v.z, PRECISION);
+		}
+		{
+			Vector v = data.getVelocity2ndDerivative(origin, Y);
+			System.out.println("2nd derivative dy=" + v);
+			assertEquals(-42.25, v.x, PRECISION);
+			assertEquals(27.5, v.y, PRECISION);
+			assertEquals(725, v.z, PRECISION);
+		}
+		{
+			Vector v = data.getVelocity2ndDerivative(origin, Z);
+			System.out.println("2nd derivative dz=" + v);
+			assertEquals(-42.0, v.x, PRECISION);
+			assertEquals(30, v.y, PRECISION);
+			assertEquals(750, v.z, PRECISION);
+		}
 	}
 
 	@Test
 	public void testVelocityLaplacian() {
 
-		Data data = new GridData(new Grid1(val(1000), val(10, 11, 12), val(20,
-				20, 20), val(100, 110, 120), val(200, 200, 200), val(1000,
-				1100, 1200), val(2000, 2000, 2000)));
+		Data data = new GridData(new Grid1(val(100, 100, 100, 1000), val(10,
+				11, 12), val(20, 20, 20), val(100, 110, 120),
+				val(200, 200, 200), val(1000, 1100, 1200),
+				val(2000, 2000, 2000)));
 		Vector v = data.getVelocityLaplacian(origin);
-		System.out.println(v);
+		System.out.println("laplacian=" + v);
 	}
 
 }
