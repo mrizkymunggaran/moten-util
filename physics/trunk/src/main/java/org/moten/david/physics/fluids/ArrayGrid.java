cdef33a455727533a88a7a200aea99d6b6b61f5e
davidmoten@gmail.com <davidmoten@gmail.com@5f29f07a-d3f3-11de-9f39-755c91dee682>
2011-05-17 05:20:34 +0000

    physics dev

--- physics/trunk/src/main/java/org/moten/david/physics/fluids/ArrayGrid.java
+++ physics/trunk/src/main/java/org/moten/david/physics/fluids/ArrayGrid.java
@@ -1,11 +1,11 @@
 package org.moten.david.physics.fluids;
 
-import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 
 import org.moten.david.util.math.Direction;
+import org.moten.david.util.math.Neighbours;
 import org.moten.david.util.math.Pair;
 import org.moten.david.util.math.Triple;
 import org.moten.david.util.math.Vector;
@@ -13,30 +13,20 @@ import org.moten.david.util.math.Vector;
 public class ArrayGrid implements Grid<Value> {
 
 	private final Map<Vector, Value> map;
-	private final Map<Triple<Vector, Direction, Boolean>, Vector> n = new HashMap<Triple<Vector, Direction, Boolean>, Vector>();
+	private final Map<Triple<Vector, Direction, Boolean>, Vector> n;
 
+	/**
+	 * Constructor. <code>map</code> should contain a grid of points all on a
+	 * discrete 3d grid so that a cell's neighbour in say the y direction has
+	 * the same x and z position. This is an important assumption in calculation
+	 * of derivatives at each point. You can think of the points then as the
+	 * centres of a mass of aligned boxes.
+	 * 
+	 * @param map
+	 */
 	public ArrayGrid(Map<Vector, Value> map) {
 		this.map = map;
-		prepareNeighbours();
-	}
-
-	private void prepareNeighbours() {
-		for (Entry<Vector, Value> entry : map.entrySet()) {
-			// brute force?
-			for (Direction direction : Direction.values()) {
-				Vector closestPositive = getClosest(map.entrySet(), entry,
-						direction, true);
-				Vector closestNegative = getClosest(map.entrySet(), entry,
-						direction, false);
-				// TODO
-			}
-		}
-	}
-
-	private Vector getClosest(Set<Entry<Vector, Value>> entrySet,
-			Entry<Vector, Value> entry, Direction direction, boolean b) {
-		// TODO Auto-generated method stub
-		return null;
+		this.n = Neighbours.findNeighbours(map.keySet());
 	}
 
 	@Override
@@ -56,8 +46,32 @@ public class ArrayGrid implements Grid<Value> {
 
 	@Override
 	public Iterable<Pair<Vector, Value>> entries() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		return new Iterable<Pair<Vector, Value>>() {
 
+			@Override
+			public Iterator<Pair<Vector, Value>> iterator() {
+				return new Iterator<Pair<Vector, Value>>() {
+					private final Iterator<Entry<Vector, Value>> entries = map
+							.entrySet().iterator();
+
+					@Override
+					public boolean hasNext() {
+						return entries.hasNext();
+					}
+
+					@Override
+					public Pair<Vector, Value> next() {
+						Entry<Vector, Value> entry = entries.next();
+						return new Pair<Vector, Value>(entry.getKey(),
+								entry.getValue());
+					}
+
+					@Override
+					public void remove() {
+						throw new RuntimeException("remove not implemented");
+					}
+				};
+			}
+		};
+	}
 }
--- physics/trunk/src/main/java/org/moten/david/util/math/Triple.java
+++ physics/trunk/src/main/java/org/moten/david/util/math/Triple.java
@@ -2,8 +2,57 @@ package org.moten.david.util.math;
 
 public class Triple<S, T, U> {
 
-	public Triple(S a, T b, U c) {
-		// TODO Auto-generated constructor stub
+	@Override
+	public String toString() {
+		return "Triple [value1=" + value1 + ", value2=" + value2 + ", value3="
+				+ value3 + "]";
+	}
+
+	private final S value1;
+	private final T value2;
+	private final U value3;
+
+	public Triple(S value1, T value2, U value3) {
+		this.value1 = value1;
+		this.value2 = value2;
+		this.value3 = value3;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((value1 == null) ? 0 : value1.hashCode());
+		result = prime * result + ((value2 == null) ? 0 : value2.hashCode());
+		result = prime * result + ((value3 == null) ? 0 : value3.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Triple other = (Triple) obj;
+		if (value1 == null) {
+			if (other.value1 != null)
+				return false;
+		} else if (!value1.equals(other.value1))
+			return false;
+		if (value2 == null) {
+			if (other.value2 != null)
+				return false;
+		} else if (!value2.equals(other.value2))
+			return false;
+		if (value3 == null) {
+			if (other.value3 != null)
+				return false;
+		} else if (!value3.equals(other.value3))
+			return false;
+		return true;
 	}
 
 }
