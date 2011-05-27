bb3f215a7655d4d30f066f6f00f0bfce3f78b8ce
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2011-05-27 03:04:33 +0000

    physics scala

--- physics/trunk/ns.scala
+++ physics/trunk/ns.scala
@@ -14,12 +14,16 @@ case class Vector(x:Double, y:Double, z:Double) {
       case Z => z 
     }
   }
-  def sum = x+y+z
-  def dot(v:Vector)=x*v.x+y*v.y+z*v.z 
+  def *(v:Vector) = x*v.x + y*v.y + z*v.z
+  def *(d:Double) = Vector(x*d,y*d,z*d)
+  def -(v:Vector) = Vector(x-v.x,y-v.y,z-v.z)
+  def +(v:Vector) = Vector(x+v.x,y+v.y,z+v.z)
+  def /(d:Double) = Vector(x/d,y/d,z/d)
+  def sum = x + y + z
 }
 
 case class Matrix(row1:Vector,row2:Vector,row3:Vector){
-  def *(v:Vector) = Vector(row1.dot(v),row2.dot(v),row3.dot(v))
+  def *(v:Vector) = Vector(row1*v, row2*v, row3*v)
 }
 
 case class Value (
@@ -31,16 +35,36 @@ case class Value (
 case class Entry (position:Vector, value:Value)
 
 trait Data {
-  def pressure:Vector=>Double = {v:Vector=>value(v).pressure} 
-  def velocity:Vector=>Vector = {v:Vector=>value(v).velocity}
+  def pressure:Vector=>Double = value(_).pressure 
+  def velocity:Vector=>Vector = value(_).velocity
+  def velocity(direction:Direction):Vector=>Double 
+      = value(_).velocity.get(direction)
   def value(vector:Vector):Value
   def gradient(f:Vector=>Double, position:Vector):Vector
   def gradient2nd(f:Vector=>Double, position:Vector):Vector
   def laplacian(f:Vector=>Double, position:Vector)
       = gradient2nd(f,position).sum
+  def velocityLaplacian(position:Vector):Vector  
+      = Vector(laplacian(velocity(_).x,position),
+               laplacian(velocity(_).y,position),
+               laplacian(velocity(_).z,position))   
+  def velocityJacobian(position:Vector)
+      = Matrix(gradient(velocity(X),position),
+               gradient(velocity(Y),position),
+               gradient(velocity(Z),position))
+  def getVelocityDerivativeWithTime(data:Data,position:Vector)={
+    val v = value(position)
+    val vLaplacian = velocityLaplacian(position)
+    val pressureGradient = gradient(pressure,position)
+    val vJacobian = velocityJacobian(position)
+    
+   (((vLaplacian*v.viscosity - pressureGradient)/v.density)
+      + Vector(0,0,9.8))
+      // - (vJacobian * v.velocity)
+  }
 }
 
 class NavierStokes {
- def step(data:Data,timestep:Double):Data = data 
+  def step(data:Data,timestep:Double):Data = data 
 }
 
