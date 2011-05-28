3dec29e56db51fa5e74bf1f85546ec7c0357e00b
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2011-05-28 10:55:41 +0000

    scalaphys dev

---
+++ scalaphys/trunk/src/test/scala/org/moten/david/ns/NavierStokesTest.scala
@@ -0,0 +1,16 @@
+package org.moten.david.ns
+
+import org.junit._
+import Assert._
+
+@Test
+class PhysicsTest {
+
+  @Test
+  def testOK() = assertTrue(true)
+
+  //    @Test
+  //    def testKO() = assertTrue(false)
+
+}
+
