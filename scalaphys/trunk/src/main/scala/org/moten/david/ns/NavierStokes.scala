ebedf8b77cc355c96ccffe4f7edc1a9ce055abbd
davidmoten@gmail.com <davidmoten@gmail.com@5f29f07a-d3f3-11de-9f39-755c91dee682>
2011-06-01 05:15:15 +0000

    scalaphys dev

--- scalaphys/trunk/src/main/scala/org/moten/david/ns/NavierStokes.scala
+++ scalaphys/trunk/src/main/scala/org/moten/david/ns/NavierStokes.scala
@@ -63,7 +63,11 @@ case class Matrix(row1: Vector, row2: Vector, row3: Vector) {
 case class Value(
   velocity: Vector, pressure: Double, depth: Double,
   density: Double, viscosity: Double, isWall: Boolean,
-  isBoundary: Map[Direction, Boolean])
+  isBoundary: Map[Direction, Boolean]) {
+  def modifyPressure(p: Double) = {
+    Value(velocity, p, depth, density, viscosity, isWall, isBoundary)
+  }
+}
 
 object Data {
   val gravity = Vector(0, 0, -9.8)
@@ -78,6 +82,8 @@ trait Data {
   def getPressureGradient(position: Vector): Vector
   def getVelocityGradient2nd(position: Vector, direction: Direction): Vector
   def getPressureGradient2nd(position: Vector): Vector
+  def getGradient(position: Vector, direction: Direction,
+    wallGradient: Double, boundaryGradient: Double, f: Vector => Double, isFirstDerivative: Boolean): Double
 
   //implemented for you
   def getVelocityLaplacian(position: Vector, direction: Direction) = {
@@ -106,7 +112,7 @@ trait Data {
   def getPressureLaplacian(position: Vector) = {
     getPressureGradient2nd(position).sum
   }
-  def valueAfterTimeStep(position: Vector, timeStep: Double) = {
+  def getVelocityAfterTimeStep(position: Vector, timeStep: Double) = {
     getValue(position).velocity.add(dvdt(position) * timeStep)
   }
 
@@ -114,91 +120,41 @@ trait Data {
    * Returns the Conservation of Mass (Continuity) Equation described by the
    * Navier-Stokes equations.
    */
-  private def continuityFunction(position: Vector, v1: Vector, timeDelta: Double): Double = {
-    val velocityNext = valueAfterTimeStep(position, timeDelta)
+  private def getPressureCorrection(position: Vector, v1: Vector, timeDelta: Double)(pressure: Double): Double = {
+    val velocityNext = getVelocityAfterTimeStep(position, timeDelta)
     val v = getValue(position)
     //assume not wall or boundary
-    val valueNext = Value(velocityNext, v.pressure, v.depth, v.density, v.viscosity, true, Map(X -> false, Y -> false, Z -> false))
+    val valueNext = Value(velocityNext, pressure, v.depth, v.density, v.viscosity, true, Map(X -> false, Y -> false, Z -> false))
     val data2 = new DataOverride(this, position, valueNext)
-    data2.getPressureCorrectionEquation(position)
+    data2.getPressureCorrection(position)
   }
 
-  //TODO implement
-  def getPressureCorrectionEquation(position: Vector): Double = {
+  def getPressureCorrection(position: Vector): Double = {
     val value = getValue(position)
     val pressureLaplacian = getPressureLaplacian(position)
-
-    0
+    def f(v: Vector, direction: Direction) =
+      getVelocityGradient(v, direction) * v
+    return pressureLaplacian +
+      Direction.ordered.map(d => getGradient(position, d, 0, 0, f(_, d), true)).sum
   }
 
-  //  private double f(Vector v1, Vector v2, Vector velocity1, Vector velocity2,
-  //			Direction direction) {
-  //		double f2 = getVelocityJacobian(v2).multiply(velocity2).get(direction);
-  //		double f1 = getVelocityJacobian(v1).multiply(velocity1).get(direction);
-  //		double result = (f2 - f1) / (v2.get(direction) - v1.get(direction));
-  //		return result;
-  //	}
-
-  //  public double getPressureCorrectiveFunction(Vector position) {
-  //		// solve del2 p + del.(v.del)v=0
-  //		// i.e del2 p + div (J(v)v) = 0
-  //		Value value = getValue(position);
-  //		Value wallValue = new Value(new Vector(0, 0, 0), value.pressure,
-  //				value.depth, value.density, value.viscosity);
-  //		Neighbours n = getNeighbours(position, wallValue);
-  //		double pressureLaplacian = getPressureLaplacian(position);
-  //
-  //		double resultX = f(n.x1, n.x2, n.valueX1.velocity, n.valueX2.velocity,
-  //				Direction.X);
-  //		double resultY = f(n.y1, n.y2, n.valueY1.velocity, n.valueY2.velocity,
-  //				Direction.Y);
-  //		double resultZ = f(n.z1, n.z2, n.valueZ1.velocity, n.valueZ2.velocity,
-  //				Direction.Z);
-  //		return pressureLaplacian + resultX + resultY + resultZ;
-  //	}
-
-  //  Value getValueAfterTime(Data data, Vector position, double timeDelta) {
-  //
-  //		log.fine("getting value after time=" + timeDelta);
-  //		Value value0 = data.getValue(position);
-  //		if (value0.isWall())
-  //			return value0;
-  //
-  //		log.fine("initial value:" + value0);
-  //
-  //		Vector v1 = getVelocityAfterTime(data, position, timeDelta);
-  //		log.fine("first guess velocity=" + v1);
-  //
-  //		// if stopped now then continuity (conservation of mass) equation might
-  //		// not be satisfied. Perform pressure correction:
-  //		Function<Double, Double> f = createContinuityFunction(data, position,
-  //				v1, timeDelta);
-  //
-  //		double p0 = value0.pressure;
-  //		// solve f = 0 for pressure p where f is defined:
-  //		// TODO best value for precision?
-  //		double precision = 0.001;
-  //		// TODO best value for step size?
-  //		double pressureStepSize = 1; // in Pa
-  //		Double pressure = NewtonsMethodSolver.solve(f, p0, pressureStepSize,
-  //				precision, MAX_NEWTONS_ITERATIONS);
-  //		if (pressure == null)
-  //			pressure = p0;
-  //
-  //		// substitute the pressure back into the conservation of momentum eqn to
-  //		// solve for velocity again
-  //		Value v = data.getValue(position);
-  //		Value valueNext = new Value(v.velocity, pressure, v.depth, v.density,
-  //				v.viscosity);
-  //		Data data2 = new DataOverride(data, position, valueNext);
-  //		Vector v2 = getVelocityAfterTime(data2, position, timeDelta);
-  //
-  //		Value result = new Value(v2, pressure, value0.depth, value0.density,
-  //				value0.viscosity);
-  //		log.fine("returning value=" + result);
-  //		return result;
-  //	}
-
+  /**
+   * See http://en.wikipedia.org/wiki/Pressure-correction_method
+   */
+  def getValueAfterTime(position: Vector, timeDelta: Double): Value = {
+    val value0 = getValue(position)
+    if (value0.isWall) return value0
+    val v1 = getVelocityAfterTimeStep(position, timeDelta)
+    val f = getPressureCorrection(position, v1, timeDelta)(_)
+    val h = 0.1
+    val precision = 0.0001
+    val maxIterations = 15
+    val newPressure = NewtonsMethod.solve(f, value0.pressure, h, precision, maxIterations) match {
+      case None => value0.pressure
+      case a: Some[Double] => a.get
+    }
+    return value0.modifyPressure(newPressure)
+  }
 }
 
 private class DataOverride(data: Data, position: Vector, value: Value) extends Data {
@@ -207,6 +163,9 @@ private class DataOverride(data: Data, position: Vector, value: Value) extends D
   def getPressureGradient(position: Vector): Vector = data.getPressureGradient(position)
   def getVelocityGradient2nd(position: Vector, direction: Direction): Vector = data.getVelocityGradient(position, direction)
   def getPressureGradient2nd(position: Vector): Vector = data.getPressureGradient2nd(position)
+  def getGradient(position: Vector, direction: Direction,
+    wallGradient: Double, boundaryGradient: Double,
+    f: Vector => Double, isFirstDerivative: Boolean): Double = 0
 }
 
 object RegularGridData {
@@ -215,7 +174,8 @@ object RegularGridData {
     //produce a map of Direction to a map of ordinate values with their 
     //negative and positive direction neighbour ordinate values
     Direction.values.map(d => (d, vectors.map(_.get(d))
-      .toSet.toList.sorted.sliding(3).toList.map(x => (x(1), (x(0), x(2)))).toMap)).toMap
+      .toSet.toList.sorted.sliding(3)
+      .toList.map(x => (x(1), (x(0), x(2)))).toMap)).toMap
   }
 }
 
@@ -261,7 +221,7 @@ class RegularGridData(map: Map[Vector, Value]) extends Data {
   def getVelocityGradient2nd(position: Vector, direction: Direction): Vector = {
     new Vector(Direction.ordered.map(d => getGradient(position, direction, 0, 0, getValue(_).velocity.get(d), SECOND)))
   }
-  
+
   private type Pair = Tuple2[Double, Double]
 
   private def getGradient(a1: Pair, a: Pair, a2: Pair, isFirstDerivative: Boolean): Double = {
@@ -271,7 +231,7 @@ class RegularGridData(map: Map[Vector, Value]) extends Data {
       (a2._2 + a1._2 - 2 * a._2) / (a2._1 - a1._1)
   }
 
-  private def getGradient(position: Vector, direction: Direction,
+  def getGradient(position: Vector, direction: Direction,
     wallGradient: Double, boundaryGradient: Double, f: Vector => Double, isFirstDerivative: Boolean): Double = {
     val value = getValue(position)
     if (value.isWall)
