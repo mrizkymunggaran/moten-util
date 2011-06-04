230aaf056f0be8b1c5f2a88fe1373f334e6b8044
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2011-06-04 11:40:14 +0000

    scalaphys dev

--- scalaphys/trunk/src/test/scala/org/moten/david/ns/navierStokesTest.scala
+++ scalaphys/trunk/src/test/scala/org/moten/david/ns/navierStokesTest.scala
@@ -8,12 +8,101 @@ import scala.util.Random._
 import Logger._
 
 @Test
+class VectorTest {
+
+  val Precision = 0.000001
+
+  @Test
+  def testVectorAddition() {
+    val v1 = new Vector(1, 2, 3)
+    val v2 = new Vector(10, 20, 30)
+    assertEquals(Vector(11, 22, 33), v1 add v2)
+  }
+
+  @Test
+  def testVectorSubtraction() {
+    val v1 = new Vector(1, 2, 3)
+    val v2 = new Vector(10, 20, 30)
+    assertEquals(Vector(9, 18, 27), v2 minus v1)
+    assertEquals(Vector(9, 18, 27), v2 - v1)
+  }
+
+  @Test
+  def testVectorDotProduct() {
+    val v1 = new Vector(1, 2, 3)
+    val v2 = new Vector(10, 20, 30)
+    assertEquals(140.0, v1 * v2, Precision)
+  }
+
+  @Test
+  def testVectorScalaProduct() {
+    val v1 = new Vector(1, 2, 3)
+    assertEquals(Vector(2, 4, 6), v1 * 2)
+  }
+
+  @Test
+  def testVectorScalaDivide() {
+    val v1 = new Vector(1, 2, 3)
+    assertEquals(Vector(1.0 / 2, 2 / 2.0, 3 / 2.0), v1 / 2)
+  }
+
+  @Test
+  def testVectorSum() {
+    val v1 = new Vector(1, 2, 3.5)
+    assertEquals(1 + 2 + 3.5, v1 sum, Precision)
+  }
+
+  @Test
+  def testVectorModify() {
+    val v1 = new Vector(1, 2, 3)
+    assertEquals(Vector(4, 2, 3), v1.modify(X, 4))
+    assertEquals(Vector(1, 4, 3), v1.modify(Y, 4))
+    assertEquals(Vector(1, 2, 4), v1.modify(Z, 4))
+  }
+}
+
+@Test
 class NewtonsMethodTest {
 
   @Test
-  def testNewtonsMethod() {
+  def testNewtonsMethodSolvesANonLinearEquation() {
     def f(x: Double) = sqrt(x) - 3
     NewtonsMethod.solve(f, 2, 0.1, 0.0001, 5) match {
+      case None => fail
+      case x: Some[Double] => assertEquals(9.0, x.get, 0.001)
+    }
+  }
+
+  @Test
+  def testNewtonsMethodSolvesLinearInOneIteration() {
+    def f(x: Double) = 2 * x - 3
+    NewtonsMethod.solve(f, 2, 0.1, 0.0001, 1) match {
+      case None => fail
+      case x: Some[Double] => assertEquals(1.5, x.get, 0.001)
+    }
+  }
+
+  @Test
+  def testNewtonsMethodSolvesInOneIterationIfSuppliedWithAnswerAsInitialValue() {
+    def f(x: Double) = sqrt(x) - 3
+    NewtonsMethod.solve(f, 9, 0.1, 0.0001, 1) match {
+      case None => fail
+      case x: Some[Double] => assertEquals(9.0, x.get, 0.001)
+    }
+  }
+
+  @Test
+  def testNewtonsMethodReturnsNoneIfMaxIterationsIsZeroAndIntialValueIsWrong() {
+    def f(x: Double) = sqrt(x) - 3
+    NewtonsMethod.solve(f, 8, 0.1, 0.0001, 0) match {
+      case None => Unit
+      case x: Some[Double] => fail
+    }
+  }
+  @Test
+  def testNewtonsMethodReturnsCorrectAnswerIfMaxIterationsIsZeroAndIntialValueIsCorrect() {
+    def f(x: Double) = sqrt(x) - 3
+    NewtonsMethod.solve(f, 9, 0.1, 0.0001, 0) match {
       case None => fail
       case x: Some[Double] => assertEquals(9.0, x.get, 0.001)
     }
