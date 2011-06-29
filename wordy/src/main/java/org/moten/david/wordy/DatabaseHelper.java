9feca7e2a7e20b5aa0511b0c60fc8eef46d9a492
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2011-06-29 12:21:14 +0000

    wordy database helper

---
+++ wordy/src/main/java/org/moten/david/wordy/DatabaseHelper.java
@@ -0,0 +1,37 @@
+package org.moten.david.wordy;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+	private static final String DATABASE_NAME = "wordy";
+
+	private static final int DATABASE_VERSION = 1;
+
+	// Database creation sql statement
+	private static final String DATABASE_CREATE = "create table word (word text primary key);";
+
+	public DatabaseHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	// Method is called during creation of the database
+	@Override
+	public void onCreate(SQLiteDatabase database) {
+		database.execSQL(DATABASE_CREATE);
+	}
+
+	// Method is called during an upgrade of the database, e.g. if you increase
+	// the database version
+	@Override
+	public void onUpgrade(SQLiteDatabase database, int oldVersion,
+			int newVersion) {
+		Log.w(DatabaseHelper.class.getName(),
+				"Upgrading database from version " + oldVersion + " to "
+						+ newVersion + ", which will destroy all old data");
+		database.execSQL("DROP TABLE IF EXISTS word");
+		onCreate(database);
+	}
+}
