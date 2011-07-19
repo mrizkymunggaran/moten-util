372d1985d75b3a4bdfb917a80a6646f140e0386d
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2011-07-19 13:38:44 +0000

    sowpods sqlite dev

--- sowpods-sqlite/src/main/scala/org/moten/david/sowpods/dbCreator.scala
+++ sowpods-sqlite/src/main/scala/org/moten/david/sowpods/dbCreator.scala
@@ -2,7 +2,16 @@ package org.moten.david.sowpods
 
 object DbCreator {
 
+  def main(args: Array[String]) {
+    create
+  }
+
   import scala.io.Source
+  import java.sql._
+
+  def exec(sql: String)(implicit stmt: Statement) {
+    stmt.executeUpdate(sql)
+  }
 
   def create {
     println("loading words")
@@ -13,15 +22,14 @@ object DbCreator {
       .toList.toSet.toList.sorted
 
     println("creating database")
-    import java.sql._
     Class.forName("org.sqlite.JDBC")
     val connection = DriverManager.getConnection("jdbc:sqlite:target/word.db")
-    val stmt = connection.createStatement
-    stmt.executeUpdate("drop table if exists android_metadata")
-    stmt.executeUpdate("create table android_metadata (locale TEXT DEFAULT 'en_US');")
-    stmt.executeUpdate("insert into android_metadata VALUES ('en_US');")
-    stmt.executeUpdate("drop table if exists word;")
-    stmt.executeUpdate("create table word(_id integer primary key,word,word_sorted);")
+    implicit val stmt = connection.createStatement
+    exec("drop table if exists android_metadata")
+    exec("create table android_metadata (locale TEXT DEFAULT 'en_US');")
+    exec("insert into android_metadata VALUES ('en_US');")
+    exec("drop table if exists word;")
+    exec("create table word(_id integer primary key,word,word_sorted);")
     val pstmt = connection.prepareStatement("insert into word values (?,?,?);")
     connection.setAutoCommit(false)
     var count = 0
@@ -34,16 +42,12 @@ object DbCreator {
     })
     pstmt.executeBatch
     connection.commit
-    stmt.executeUpdate("create unique index idx_word on word(word asc);")
-    stmt.executeUpdate("create index idx_word_sorted on word(word_sorted asc);")
+    exec("create unique index idx_word on word(word asc);")
+    exec("create index idx_word_sorted on word(word_sorted asc);")
     connection.commit
     connection.close
 
     println("created database")
-  }
-
-  def main(args: Array[String]) {
-    create
   }
 
 }
