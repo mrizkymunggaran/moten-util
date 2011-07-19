0abba76b2a7510e4701f27a9058b22d01eca1594
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2011-07-19 13:28:10 +0000

    sowpods sqlite dev

--- sowpods-sqlite/src/main/scala/org/moten/david/sowpods/dbCreator.scala
+++ sowpods-sqlite/src/main/scala/org/moten/david/sowpods/dbCreator.scala
@@ -21,17 +21,20 @@ object DbCreator {
     stmt.executeUpdate("create table android_metadata (locale TEXT DEFAULT 'en_US');")
     stmt.executeUpdate("insert into android_metadata VALUES ('en_US');")
     stmt.executeUpdate("drop table if exists word;")
-    stmt.executeUpdate("create table word(_id,word_sorted);")
-    val pstmt = connection.prepareStatement("insert into word values (?,?);")
+    stmt.executeUpdate("create table word(_id integer primary key,word,word_sorted);")
+    val pstmt = connection.prepareStatement("insert into word values (?,?,?);")
     connection.setAutoCommit(false)
+    var count = 0
     words.foreach(w => {
-      pstmt.setString(1, w)
-      pstmt.setString(2, w.sorted)
+      count += 1
+      pstmt.setInt(1, count)
+      pstmt.setString(2, w)
+      pstmt.setString(3, w.sorted)
       pstmt.addBatch
     })
     pstmt.executeBatch
     connection.commit
-    stmt.executeUpdate("create unique index idx_word on word(_id asc);")
+    stmt.executeUpdate("create unique index idx_word on word(word asc);")
     stmt.executeUpdate("create index idx_word_sorted on word(word_sorted asc);")
     connection.commit
     connection.close
