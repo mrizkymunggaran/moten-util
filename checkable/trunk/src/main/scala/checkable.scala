6f57092997804279622a87b6d616f17737c55b2e
davidmoten <davidmoten@5f29f07a-d3f3-11de-9f39-755c91dee682>
2011-11-15 08:40:58 +0000

    checkable dev

--- checkable/trunk/src/main/scala/checkable.scala
+++ checkable/trunk/src/main/scala/checkable.scala
@@ -1,15 +1,29 @@
 package checkable {
 
-  import java.util.Properties
   import scala.collection.JavaConversions._
   import java.net.URL
+  import scala.collection.immutable.Map
 
-  trait Source;
+  object Util {
 
-  case class FunctionValue(value: Boolean, properties: Map[String, String])
+    def propertiesToMap(url: String) = {
+      val p = new java.util.Properties();
+      val is = new URL(url).openStream();
+      p.load(is);
+      is.close
+      p.entrySet()
+        .map(x => (x.getKey.toString, x.getValue.toString))
+        .toMap
+    }
 
-  trait Function {
-    def apply(): FunctionValue
+    type Properties = Map[String, String]
+  }
+
+  import Util.Properties
+
+  case class FunctionValue(value: Boolean, properties: Properties)
+
+  trait Function extends Function0[FunctionValue] {
 
     def and(f: Function) = {
       val v = apply
@@ -29,24 +43,26 @@ package checkable {
     }
   }
 
-  trait NumericExpression extends Function1[Unit, BigDecimal] {
-    def >(n: NumericExpression): Function1[Unit, Boolean] =
-      (Unit) => apply() > n.apply()
-    def <(n: NumericExpression): Function1[Unit, Boolean] =
-      (Unit) => apply() < n.apply()
-    def >=(n: NumericExpression): Function1[Unit, Boolean] =
-      (Unit) => apply() >= n.apply()
-    def <=(n: NumericExpression): Function1[Unit, Boolean] =
-      (Unit) => apply() <= n.apply()
-    def *(n: NumericExpression): Function1[Unit, BigDecimal] =
-      (Unit) => apply() * n.apply()
-    def /(n: NumericExpression): Function1[Unit, BigDecimal] =
-      (Unit) => apply() / n.apply()
+  trait NumericExpression extends Function0[BigDecimal] {
+
+    def >(n: NumericExpression): Function0[Boolean] =
+      () => apply() > n.apply()
+    def <(n: NumericExpression): Function0[Boolean] =
+      () => apply() < n.apply()
+    def >=(n: NumericExpression): Function0[Boolean] =
+      () => apply() >= n.apply()
+    def <=(n: NumericExpression): Function0[Boolean] =
+      () => apply() <= n.apply()
+    def *(n: NumericExpression): Function0[BigDecimal] =
+      () => apply() * n.apply()
+    def /(n: NumericExpression): Function0[BigDecimal] =
+      () => apply() / n.apply()
   }
 
-  trait BooleanExpression extends Function1[Unit, Boolean]
+  trait BooleanExpression extends Function0[Boolean]
 
   trait Level
+
   case class Ok extends Level
   case class Warning extends Level
   case class Failure extends Level
@@ -65,10 +81,23 @@ package checkable {
   }
 
   trait PropertiesFunction {
-    def apply(properties: Map[String, String]): FunctionValue
+    def apply(properties: Properties): FunctionValue
+
+    def getStringMandatory(properties: Properties, key: String) =
+      properties.getOrElse(key,
+        throw new RuntimeException("value not found for key=" + key))
+
+    def getIntegerMandatory(properties: Properties, key: String) =
+      getStringMandatory(properties, key).toInt
+
+    def getLongMandatory(properties: Properties, key: String) =
+      getStringMandatory(properties, key).toLong
+
+    def getBigDecimalMandatory(properties: Properties, key: String) =
+      BigDecimal(getStringMandatory(properties, key))
   }
 
-  trait PropertiesProvider extends Function1[Unit, Map[String, String]]
+  trait PropertiesProvider extends Function0[Properties]
 
   class UrlPropertiesFunction(
     provider: PropertiesProvider,
@@ -77,25 +106,16 @@ package checkable {
   }
 
   class UrlPropertiesProvider(url: String) extends PropertiesProvider {
-    def get: Map[String, String] = {
-      val p = new Properties();
-      val is = new URL(url).openStream();
-      p.load(is);
-      is.close
-      p.entrySet()
-        .map(x => (x.getKey.toString, x.getValue.toString))
-        .toMap
-    }
+    def apply(): Properties = Util.propertiesToMap(url)
   }
 
-  case class MySource(name: String) extends Source
-
   class ExampleFunction extends PropertiesFunction {
-    def apply(p: Map[String, String]) = {
+    def apply(p: Properties) = {
       p.get("example.interval.ms") match {
         case None => FunctionValue(false, p)
         case x => FunctionValue(x == "100", p)
       }
     }
   }
+
 }
